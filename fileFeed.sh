905e2174fc3d9f3b48d7ded6a8699f3e97d4633d
Xi <xiz13@uci.edu>
2016-05-24 16:13:28 -0700

    add test data

--- fileFeed.sh
+++ fileFeed.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sbt "project noah" "run-main edu.uci.ics.cloudberry.noah.FileFeedDriver -src /home/sony/incubator-asterixdb/asterixdb/asterix-app/data/twitter/tw_messages.adm -url 172.17.0.3 -p 10001 -w 500 -b 50 -c 1000"
+sbt "project noah" "run-main edu.uci.ics.cloudberry.noah.FileFeedDriver -src noah/src/main/resources/tw_messages.adm -url 172.17.0.3 -p 10001 -w 500 -b 50 -c 1000"
