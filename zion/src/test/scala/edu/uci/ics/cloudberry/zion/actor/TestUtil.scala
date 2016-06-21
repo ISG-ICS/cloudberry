b2ea278e8229eb6bef42e0ac65a8b298bcc8eb9c
Jianfeng Jia <jianfeng.jia@gmail.com>
2016-06-21 00:08:21 -0700

    Use single thread pool to test travis (#93)
    * use single thread pool to try travis
    
    * re-enable travis cache
    
    * change dependency order

--- zion/src/test/scala/edu/uci/ics/cloudberry/zion/actor/TestUtil.scala
+++ zion/src/test/scala/edu/uci/ics/cloudberry/zion/actor/TestUtil.scala
@@ -3,7 +3,6 @@ package edu.uci.ics.cloudberry.zion.actor
 import edu.uci.ics.cloudberry.zion.asterix.AsterixConnection
 import edu.uci.ics.cloudberry.zion.common.Config
 import edu.uci.ics.cloudberry.zion.model.KeyCountPair
-import org.mockito.AdditionalAnswers
 import org.mockito.Mockito._
 import org.mockito.invocation.InvocationOnMock
 import org.mockito.stubbing.Answer
