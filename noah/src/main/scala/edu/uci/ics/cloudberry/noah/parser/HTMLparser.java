bd7b3e69e92bc9b48b8f7b4491ebf3d09354b555
Dennis Qi <dennis09121111@gmail.com>
2016-06-28 14:43:38 -0700

    dded HTMLparser, HTMLparserTest, and stat100.html

--- noah/src/main/scala/edu/uci/ics/cloudberry/noah/parser/HTMLparser.java
+++ noah/src/main/scala/edu/uci/ics/cloudberry/noah/parser/HTMLparser.java
@@ -1,7 +1,39 @@
 package edu.uci.ics.cloudberry.noah.parser;
 
 /**
- * Created by dennis126 on 06/28/2016.
+ * Created by Zhenfeng Qi on 06/28/2016.
  */
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import java.io.*;
+import java.io.IOException;
+
 public class HTMLparser {
+
+//    public static void main(String[] args)throws IOException{
+//        System.out.println("hello");
+//        String result = parse("C:\\Users\\dennis126\\Desktop\\Summer2017\\lab\\mycloudberry\\cloudberry\\noah\\src\\test\\resources\\state100.html");
+//        System.out.println(result);
+//    }
+
+    public static String parse(String dirc)throws IOException{
+        File f = new File(dirc);
+        String info = null;
+        if(f.isFile()){
+            try{
+                Document doc = Jsoup.parse(f, "UTF-8", "http://example.com/");
+                info = doc.getElementById("preview").text();
+                if(info.intern()==""){
+                    throw new Exception("empty file");
+                }
+                return info;
+            }catch (Exception e){
+
+            }
+        }else {
+            System.out.println("This is not A File");
+        }
+        return info;
+    }
 }
--- noah/src/test/scala/edu/uci/ics/cloudberry/noah/parserTest/HTMLparserTest.java
+++ noah/src/test/scala/edu/uci/ics/cloudberry/noah/parserTest/HTMLparserTest.java
@@ -1,7 +1,20 @@
 package edu.uci.ics.cloudberry.noah.parserTest;
 
+import edu.uci.ics.cloudberry.noah.parser.HTMLparser;
+
+import java.io.IOException;
+
 /**
  * Created by dennis126 on 06/28/2016.
  */
+
+import org.junit.Test;
+
 public class HTMLparserTest {
+    @Test
+    public void test() throws IOException {
+        HTMLparser hp = new HTMLparser();
+        String result = hp.parse("C:\\Users\\dennis126\\Desktop\\Summer2017\\lab\\mycloudberry\\cloudberry\\noah\\src\\test\\resources\\state100.html");
+        System.out.println(result);
+    }
 }
