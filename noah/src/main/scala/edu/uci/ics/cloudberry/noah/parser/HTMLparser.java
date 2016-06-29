a3cfd9269b74123d18ee36957d829636acd01f75
Dennis Qi <dennis09121111@gmail.com>
2016-06-29 12:58:17 -0700

    formated, testParser added, cleared unused code, and deleted http://example.com

--- noah/src/main/scala/edu/uci/ics/cloudberry/noah/parser/HTMLparser.java
+++ noah/src/main/scala/edu/uci/ics/cloudberry/noah/parser/HTMLparser.java
@@ -1,37 +1,26 @@
 package edu.uci.ics.cloudberry.noah.parser;
 
-/**
- * Created by Zhenfeng Qi on 06/28/2016.
- */
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import java.io.*;
-import java.io.IOException;
 
 public class HTMLparser {
 
-//    public static void main(String[] args)throws IOException{
-//        System.out.println("hello");
-//        String result = parse("C:\\Users\\dennis126\\Desktop\\Summer2017\\lab\\mycloudberry\\cloudberry\\noah\\src\\test\\resources\\state100.html");
-//        System.out.println(result);
-//    }
-
-    public static String parse(String dirc)throws IOException{
+    public static String parse(String dirc) throws IOException {
         File f = new File(dirc);
         String info = null;
-        if(f.isFile()){
-            try{
-                Document doc = Jsoup.parse(f, "UTF-8", "http://example.com/");
+        if (f.isFile()) {
+            try {
+                Document doc = Jsoup.parse(f, "UTF-8");
                 info = doc.getElementById("preview").text();
-                if(info.intern()==""){
+                if (info.intern() == "") {
                     throw new Exception("empty file");
                 }
                 return info;
-            }catch (Exception e){
-
+            } catch (Exception e) {
+                System.out.println("catch an exception");
             }
-        }else {
+        } else {
             System.out.println("This is not A File");
         }
         return info;
--- noah/src/test/scala/edu/uci/ics/cloudberry/noah/parserTest/HTMLparserTest.java
+++ noah/src/test/scala/edu/uci/ics/cloudberry/noah/parserTest/HTMLparserTest.java
@@ -1,18 +1,12 @@
 package edu.uci.ics.cloudberry.noah.parserTest;
 
 import edu.uci.ics.cloudberry.noah.parser.HTMLparser;
-
 import java.io.IOException;
-
-/**
- * Created by dennis126 on 06/28/2016.
- */
-
 import org.junit.Test;
 
 public class HTMLparserTest {
     @Test
-    public void test() throws IOException {
+    public void testParser() throws IOException {
         HTMLparser hp = new HTMLparser();
         //the html file location
         String result = hp.parse("C:\\Users\\dennis126\\Desktop\\Summer2017\\lab\\mycloudberry\\cloudberry\\noah\\src\\test\\resources\\state100.html");
