3eed7c7ef87858e47eee92bb15b27cf6f3658066
akapse <akapse23@gmail.com>
2016-07-01 22:20:14 -0700

    BFS

--- noah/src/main/scala/edu/uci/ics/cloudberry/noah/crawler/CrawlerDriver.scala
+++ noah/src/main/scala/edu/uci/ics/cloudberry/noah/crawler/CrawlerDriver.scala
@@ -29,7 +29,6 @@ object CrawlerDriver extends App{
     val InsertRandomDataForms = conf.getBoolean("crawljax.insertrandomdataforms")
 
     /* Configuration */
-
     val builder = CrawljaxConfiguration.builderFor(EntryURL)
 
     /* To stay with in the domain */
@@ -53,17 +52,22 @@ object CrawlerDriver extends App{
     /* Set crawlFrame to true for every case. This ensures the crawling in order. */
     builder.crawlRules().crawlFrames(true)
 
-    /* Set the maximum depth the crawler can crawler (default is 2, 0 is infinite)*/
-    builder.setMaximumDepth(conf.getInt("crawljax.maxdepth"))
+    /* Set unlimited crawl states */
+    builder.setMaximumDepth(0)
+
+    /* Set unlimited run time */
+    builder.setMaximumRunTime(0, TimeUnit MILLISECONDS)
+
+    builder.crawlRules().clickOnce(true)
+
 
     if(conf.hasPath("input")) {
-      val in = new InputSpecification();
+      val in = new InputSpecification()
       in.field(conf.getString("input.field")).setValue(conf.getString("input.value"))
       builder.crawlRules().setInputSpec(in)
     }
 
     /* Get the dont click elements to be configured */
-
     if(conf.hasPath("builder.crawlrules.dontclickelements")) {
       /* DIV */
       val DivIds = conf.getStringList("builder.crawlrules.dontclickelements.div.id")
@@ -117,18 +121,11 @@ object CrawlerDriver extends App{
     }
 
     if(conf.hasPath("builder.crawlrules.clickelements")) {
-      val AIds = conf.getStringList("builder.crawlrules.clickelements.a.id")
       val AClasses = conf.getStringList("builder.crawlrules.clickelements.a.class")
       val AText = conf.getStringList("builder.crawlrules.clickelements.a.text")
       val AHref = conf.getStringList("builder.crawlrules.clickelements.a.href")
 
-      var it = AIds.iterator()
-
-      while(it.hasNext){
-        builder.crawlRules().click("a").withAttribute("id", it.next())
-      }
-
-      it = AClasses.iterator()
+      var it = AClasses.iterator()
       while(it.hasNext) {
         builder.crawlRules().click("a").withAttribute("class", it.next())
       }
