bbf7d6acc5a74b85104cffa02080b933c3564541
Monique <moniquemoledo@hotmail.com>
2016-07-06 13:53:38 -0700

    Add readme and keywords files

--- streamFeed.sh
+++ streamFeed.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 sbt "project noah" "run-main edu.uci.ics.cloudberry.noah.feed.TwitterFeedStreamDriver \
--ck Your customer key \
--cs Your customer secret \
--loc -124.871654,32.918898,-63.627129,49.396314 \
--tk Your token \
+-ck
+qy2ThJZg2UbLHxVmuelHtneBT
+-cs
+lSV8IK5cX0TBCRHwzILfxb36WiR98T2cNIR6fHK7EupjLmOuQS
+-tk
+115418371-d7hqanVeHwdP8BC0uFOIw5WFdo8zOdMkn8kGqfFb
+-ts
+Yi7tOUI1TKH6wRKBJvE4HiJaHyVUVLHGVaIl4WE2FRbLJ
+-fo
 -tr trump \
--ts Your token secret \
 -u 127.0.0.1 -p 10001 -w 0 -b 50"
