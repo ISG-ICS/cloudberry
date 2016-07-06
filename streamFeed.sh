7e89bc35a9415afac0fcc38b323787703753e297
Monique Moledo <moniquemoledo@hotmail.com>
2016-07-06 15:35:05 -0700

    Update streamFeed.sh

--- streamFeed.sh
+++ streamFeed.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 sbt "project noah" "run-main edu.uci.ics.cloudberry.noah.feed.TwitterFeedStreamDriver \
 -ck
-qy2ThJZg2UbLHxVmuelHtneBT
+Your Consumer Key
 -cs
-lSV8IK5cX0TBCRHwzILfxb36WiR98T2cNIR6fHK7EupjLmOuQS
+Your Consumer Secret
 -tk
-115418371-d7hqanVeHwdP8BC0uFOIw5WFdo8zOdMkn8kGqfFb
+Your Access token
 -ts
-Yi7tOUI1TKH6wRKBJvE4HiJaHyVUVLHGVaIl4WE2FRbLJ
+Your Acces Secret Token
 -fo
 -tr trump \
 -u 127.0.0.1 -p 10001 -w 0 -b 50"
