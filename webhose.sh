07c88d7221c73ae0bec0ee7caaa1dae18f07b0e7
kyle01ma <kyle01ma@gmail.com>
2016-07-18 16:32:57 -0700

    fix project name bug

--- webhose.sh
+++ webhose.sh
@@ -4,7 +4,7 @@ start_time=$(($current_time-3600000))
 api_key=PLEASE-GIVE-API-KEY
 output_path=PLEASH-GIVE-OUTPUT-FILE-PATH
 
-sbt "project noah" "run-main edu.uci.ics.cloudberry.noah.news.WebHostCollector \
+sbt "project noah" "run-main edu.uci.ics.cloudberry.noah.news.WebhoseCollector \
 -a $api_key \
 -o $output_path \
 -t $start_time
