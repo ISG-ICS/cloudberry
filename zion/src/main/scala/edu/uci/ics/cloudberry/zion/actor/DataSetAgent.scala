a3777b7005997f7ab9e4e0ea7f54b985cf1ea953
Jianfeng Jia <jianfeng.jia@gmail.com>
2016-08-18 11:14:56 -0700

    rewrite the DataSetAgent using state

--- zion/src/main/scala/edu/uci/ics/cloudberry/zion/actor/DataSetAgent.scala
+++ zion/src/main/scala/edu/uci/ics/cloudberry/zion/actor/DataSetAgent.scala
@@ -1,6 +1,6 @@
 package edu.uci.ics.cloudberry.zion.actor
 
-import akka.actor.{Actor, ActorLogging, ActorRef, Props, Stash}
+import akka.actor.{Actor, ActorLogging, Props, Stash}
 import edu.uci.ics.cloudberry.zion.model.datastore.{IDataConn, IQLGenerator}
 import edu.uci.ics.cloudberry.zion.model.schema.{AppendView, Query, Schema}
 import org.joda.time.DateTime
@@ -11,32 +11,32 @@ class DataSetAgent(val schema: Schema, val queryParser: IQLGenerator, val conn: 
                   (implicit ec: ExecutionContext)
   extends Actor with Stash with ActorLogging {
 
-  var isUpdating = false
-
-  override def receive: Receive = {
+  def querying: Receive = {
     case query: Query =>
       val curSender = sender()
       conn.postQuery(queryParser.generate(query, schema)).map(curSender ! _)
-    case append: AppendView =>
-      println(s"receive from $sender() at ${DateTime.now}")
-      if (isUpdating) {
-        println("stashed")
-        stash()
-      } else {
-        isUpdating = true
-        val curSender = sender()
-        println(s"send query of $curSender at ${DateTime.now}")
-        conn.postControl(queryParser.generate(append, schema)).map { result =>
-          println(s"response to $curSender at ${DateTime.now}")
-          curSender ! result
-          self ! DataSetAgent.DoneUpdating
-        }
-      }
-    case DataSetAgent.DoneUpdating =>
-      isUpdating = false
-      unstashAll()
-    case _ =>
   }
+
+  def normal: Receive = querying orElse {
+    case append: AppendView =>
+      val curSender = sender()
+      println(s"send query of $curSender at ${DateTime.now}")
+      conn.postControl(queryParser.generate(append, schema)).map { result =>
+        println(s"response to $curSender at ${DateTime.now}")
+        curSender ! result
+        self ! DataSetAgent.DoneUpdating
+      }
+      context.become(updating)
+  }
+
+  def updating: Receive = querying orElse {
+    case append: AppendView => stash()
+    case DataSetAgent.DoneUpdating =>
+      unstashAll()
+      context.become(normal)
+  }
+
+  override def receive: Receive = normal
 }
 
 object DataSetAgent {
