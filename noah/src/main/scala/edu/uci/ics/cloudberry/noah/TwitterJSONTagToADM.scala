4dc40000bd11771c0a2d4983e175528fa2c042a7
Jianfeng Jia <jianfeng.jia@gmail.com>
2016-10-17 01:28:39 -0700

    add the multithread option to geotaging (#208)

--- noah/src/main/scala/edu/uci/ics/cloudberry/noah/TwitterJSONTagToADM.scala
+++ noah/src/main/scala/edu/uci/ics/cloudberry/noah/TwitterJSONTagToADM.scala
@@ -1,6 +1,7 @@
 package edu.uci.ics.cloudberry.noah
 
 import java.io.File
+import java.util.concurrent.Executors
 
 import edu.uci.ics.cloudberry.gnosis._
 import edu.uci.ics.cloudberry.noah.adm.Tweet
@@ -8,12 +9,16 @@ import edu.uci.ics.cloudberry.util.Profile._
 import twitter4j.TwitterObjectFactory
 
 import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+import scala.concurrent.{Await, ExecutionContext, Future}
 
 object TwitterJSONTagToADM {
   // TODO reserve all the fields and just replace the date and geo location to different part.
   // and only change it to the accept asterix json format, then we can use the feed in Asterix to transfer the format
   // without to much effort.
   val shapeMap = mutable.Map.empty[TypeLevel, String]
+  var threadNumber = 2
+  var isDebug = false
 
   val usage =
     """
@@ -28,6 +33,8 @@ object TwitterJSONTagToADM {
       case "-state" :: value :: tail => shapeMap += StateLevel -> value; parseOption(tail)
       case "-county" :: value :: tail => shapeMap += CountyLevel -> value; parseOption(tail)
       case "-city" :: value :: tail => shapeMap += CityLevel -> value; parseOption(tail)
+      case "-thread" :: value :: tail => threadNumber = value.toInt; parseOption(tail)
+      case "-debug" :: value :: tail => isDebug = true; parseOption(tail)
       case option :: tail => System.err.println("unknown option:" + option); System.err.println(usage); System.exit(1);
     }
   }
@@ -38,8 +45,10 @@ object TwitterJSONTagToADM {
       if (adm.length > 0) println(adm)
     } catch {
       case e: Throwable => {
-        e.printStackTrace(System.err)
-        System.err.println(ln)
+        if (isDebug) {
+          e.printStackTrace(System.err)
+          System.err.println(ln)
+        }
       }
     }
   }
@@ -50,8 +59,21 @@ object TwitterJSONTagToADM {
     val usGeoGnosis = profile("loading resource") {
       new USGeoGnosis(shapeMap.mapValues(new File(_)).toMap)
     }
+
+    val thpool = Executors.newFixedThreadPool(threadNumber)
+    implicit val ec: ExecutionContext = ExecutionContext.fromExecutor(thpool)
+
+    val buffer = new ArrayBuffer[String](threadNumber)
+
     for (ln <- scala.io.Source.stdin.getLines()) {
-        tagOneTweet(ln, usGeoGnosis)
+      buffer += ln
+      if (buffer.size == threadNumber) {
+        val f = Future.traverse(buffer) { tw => Future(tagOneTweet(tw, usGeoGnosis)) }
+        Await.result(f, scala.concurrent.duration.Duration.Inf)
+        buffer.clear()
+      }
     }
+    buffer.foreach(tagOneTweet(_, usGeoGnosis))
+    thpool.shutdownNow()
   }
 }
