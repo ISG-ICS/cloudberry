6f561755deb64bced7e513d2ab395fa09cd16960
Jianfeng Jia <jianfeng.jia@gmail.com>
2016-10-17 18:57:19 -0700

    Adapt the query range back to 2015 (#210)
    * bring the date to 2015
    * fix the time serial margin

--- neo/public/javascripts/common/services.js
+++ neo/public/javascripts/common/services.js
@@ -1,9 +1,10 @@
 angular.module('cloudberry.common', [])
   .service('Asterix', function($http, $timeout, $location) {
-    var startDate = new Date(2016, 5, 30, 0, 0, 0, 0);
+    var startDate = new Date(2015, 10, 22, 0, 0, 0, 0);
     var ws = new WebSocket("ws://" + $location.host() + ":" + $location.port() + "/ws");
     var asterixService = {
 
+      startDate: startDate,
       parameters: {
         dataset: "twitter.ds_tweet",
         keywords: [],
