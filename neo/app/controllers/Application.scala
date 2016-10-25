84d346498471d4f69876cab889c3929203b3ae34
Zongheng Ma <Zongheng Ma>
2016-10-25 16:42:30 -0700

    Calculate centroids of cities in the middleware when it starts

--- neo/app/controllers/Application.scala
+++ neo/app/controllers/Application.scala
@@ -1,11 +1,13 @@
 package controllers
 
+import java.io.{BufferedReader, File, FileInputStream, FileReader}
 import javax.inject.{Inject, Singleton}
 
 import actor.{NeoActor, NeoActor$}
 import akka.actor.{Actor, ActorSystem, DeadLetter, Props}
 import akka.pattern.ask
 import akka.stream.Materializer
+import akka.stream.scaladsl.Source
 import akka.util.Timeout
 import db.Migration_20160814
 import edu.uci.ics.cloudberry.zion.actor.{BerryClient, DataStoreManager}
@@ -15,7 +17,7 @@ import edu.uci.ics.cloudberry.zion.model.impl.{AQLGenerator, JSONParser, QueryPl
 import edu.uci.ics.cloudberry.zion.model.schema.Query
 import models.UserRequest
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
-import play.api.libs.json.{JsError, JsValue}
+import play.api.libs.json._
 import play.api.libs.streams.ActorFlow
 import play.api.libs.ws.WSClient
 import play.api.mvc._
@@ -47,6 +49,40 @@ class Application @Inject()(val wsClient: WSClient,
   val listener = system.actorOf(Props(classOf[Listener], this))
   system.eventStream.subscribe(listener, classOf[DeadLetter])
 
+  //start loading city data
+  val file = environment.getFile("/public/data/city.sample.json")
+  val stream = new FileInputStream(file)
+  val json = Json.parse(stream)
+  val values = (json \ "features").as[List[JsValue]]
+  for(v <- values) {
+    val tp = (v \ "geometry" \ "type").as[String]
+    var sumX = 0.0
+    var sumY = 0.0
+    var length = 0.0
+    if(tp == "Polygon") {
+      val rawCorr = (v \ "geometry" \ "coordinates").as[JsArray]
+      val realCorr = rawCorr.apply(0).as[JsArray]
+      for(i <- 0 to (realCorr.value.size - 1)){
+        sumX += realCorr.apply(i).apply(0).as[Double]
+        sumY += realCorr.apply(i).apply(1).as[Double]
+      }
+      length = realCorr.value.size
+    } else if(tp == "MultiPolygon") {
+      val allCorr = (v \ "geometry" \ "coordinates").as[JsArray]
+      for(i <- 0 to (allCorr.value.size - 1)){
+        val rawCorr = allCorr.apply(i).as[JsArray]
+        val realCorr = rawCorr.apply(0).as[JsArray]
+        for(j <- 0 to (realCorr.value.size - 1)){
+          sumX += realCorr.apply(j).apply(0).as[Double]
+          sumY += realCorr.apply(j).apply(1).as[Double]
+        }
+        length += realCorr.value.size
+      }
+    }
+    println(length,sumX,sumY)
+    //TODO: store the midpoints in order
+  }
+
   def index = Action {
     Ok(views.html.index("Cloudberry"))
   }
@@ -70,6 +106,11 @@ class Application @Inject()(val wsClient: WSClient,
     wsClient.url(url).get().map { response =>
       Ok(response.json)
     }
+  }
+
+  def city(NELat: String, SWLat: String, NELng: String, SWLng: String) = Action {
+//TODO: Do binary search through the midpoints and return the satisfied ones
+    Ok("test");
   }
 
   def berryQuery = Action.async(parse.json) { request =>
