869a7e12aacb1ca46927165c995d95b00f620d31
Zongheng Ma <zonghengma@gmail.com>
2016-11-03 16:54:10 -0700

    Test for the citylevel

---
+++ neo/test/controllers/ApplicationTest.scala
@@ -0,0 +1,19 @@
+package controllers
+
+import java.io.File
+
+import org.specs2.mutable.Specification
+
+/**
+  * Created by zongh on 10/26/2016.
+  */
+class ApplicationTest extends Specification {
+
+  "application" should {
+    "load the city data from a file" in {
+      val cities = Application.loadCity(new File("C:\\Users\\zongh\\OneDrive\\Documents\\UCI\\cloudberry\\cloudberry\\neo\\public\\data\\city.json"))
+      cities.size must_== 233
+
+    }
+  }
+}
