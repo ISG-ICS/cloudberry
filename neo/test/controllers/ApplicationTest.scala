3d25e85b7d27c6c94bbf2cee0563b2c15c2f45ae
Zongheng Ma <zonghengma@gmail.com>
2016-11-04 12:25:55 -0700

    new test

--- neo/test/controllers/ApplicationTest.scala
+++ neo/test/controllers/ApplicationTest.scala
@@ -11,8 +11,15 @@ class ApplicationTest extends Specification {
 
   "application" should {
     "load the city data from a file" in {
-      val cities = Application.loadCity(new File("C:\\Users\\zongh\\OneDrive\\Documents\\UCI\\cloudberry\\cloudberry\\neo\\public\\data\\city.json"))
+      val cities = Application.loadCity(new File("neo/public/data/city.json"))
       cities.size must_== 29834
+    }
+
+    "calculate centroid" in {
+      val cities = Application.loadCity(new File("neo/public/data/city.sample.json"))
+      cities.size must_== 1006
+      (cities.apply(0) \ "centroidX").as[Double] must_== -176.6287565
+      (cities.apply(0) \ "centroidY").as[Double] must_== 51.879214000000005
     }
   }
 }
