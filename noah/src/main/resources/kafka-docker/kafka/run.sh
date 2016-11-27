ea303ed0413b0f0faf735354f5b47e41337741d5
Xi Zhang <xiz@walnut.ics.uci.edu>
2016-11-26 16:44:34 -0800

    fix bash issus

--- noah/src/main/resources/kafka-docker/kafka/run.sh
+++ noah/src/main/resources/kafka-docker/kafka/run.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 docker build . -t kafka
-sudo mkdir /home/kafka
-docker run --detach -v /home/kafka/:/opt/kafka/data -p 9092:9092 kafka
+sudo mkdir /home/kafka-data
+docker run --detach -v /home/kafka-data/:/opt/kafka/data -p 9092:9092 kafka
