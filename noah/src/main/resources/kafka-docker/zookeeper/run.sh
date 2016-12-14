0edbbefebb5271c670801665299a557ffe05981c
Xi Zhang <xiz13@uci.edu>
2016-12-13 21:52:36 -0800

    add a change

--- noah/src/main/resources/kafka-docker/zookeeper/run.sh
+++ noah/src/main/resources/kafka-docker/zookeeper/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-docker build . -t zookeeper
-sudo mkdir /home/zookeeper
-docker run --detach -v /home/zookeeper/:/opt/zookeeper/data/ -p 2181:2181 zookeeper
+
+VOLUME_DIR=/home/zookeeper
+sudo mkdir $VOLUME_DIR
+docker run --detach -v $VOLUME_DIR:/opt/zookeeper/data/ -p 2181:2181 xizzz/zookeeper
