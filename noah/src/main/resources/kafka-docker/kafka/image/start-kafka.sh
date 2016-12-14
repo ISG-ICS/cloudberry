70beda327e9a710c612ed40fbfa3457a322eebb0
Xi Zhang <xiz13@uci.edu>
2016-12-13 21:49:39 -0800

    rewrite kafka-docker

---
+++ noah/src/main/resources/kafka-docker/kafka/image/start-kafka.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+export KAFKA_ADVERTISED_LISTENERS=$1
+export KAFKA_BROKER_ID=$2
+export KAFKA_ZOOKEEPER_CONNECT=$3
+
+export KAFKA_DEFAULT_REPLICATION_FACTOR=2
+export KAFKA_NUM_PARTITIONS=3
+export KAFKA_LOG_RETENTION_HOURS=-1
+
+for VAR in `env`
+do
+  if [[ $VAR =~ ^KAFKA_ && ! $VAR =~ ^KAFKA_HOME ]]; then
+    kafka_name=`echo "$VAR" | sed -r "s/KAFKA_(.*)=.*/\1/g" | tr '[:upper:]' '[:lower:]' | tr _ .`
+    env_var=`echo "$VAR" | sed -r "s/(.*)=.*/\1/g"`
+    if egrep -q "(^|^#)$kafka_name=" $KAFKA_HOME/config/server.properties; then
+        sed -r -i "s@(^|^#)($kafka_name)=(.*)@\2=${!env_var}@g" $KAFKA_HOME/config/server.properties #note that no config values may contain an '@' char
+    else
+        echo "$kafka_name=${!env_var}" >> $KAFKA_HOME/config/server.properties
+    fi
+  fi
+done
+
+if [[ -n "$CUSTOM_INIT_SCRIPT" ]] ; then
+  eval $CUSTOM_INIT_SCRIPT
+fi
+
+
+KAFKA_PID=0
+
+# see https://medium.com/@gchudnov/trapping-signals-in-docker-containers-7a57fdda7d86#.bh35ir4u5
+term_handler() {
+  echo 'Stopping Kafka....'
+  if [ $KAFKA_PID -ne 0 ]; then
+    kill -s TERM "$KAFKA_PID"
+    wait "$KAFKA_PID"
+  fi
+  echo 'Kafka stopped.'
+  exit
+}
+
+
+# Capture kill requests to stop properly
+trap "term_handler" SIGHUP SIGINT SIGTERM
+$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties &
+KAFKA_PID=$!
+
+wait "$KAFKA_PID"
--- noah/src/main/resources/kafka-docker/kafka/run.sh
+++ noah/src/main/resources/kafka-docker/kafka/run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-docker build . -t kafka
-sudo mkdir /home/kafka-data
-docker run --detach -v /home/kafka-data/:/opt/kafka/data -p 9092:9092 kafka
+
+KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://kiwi.ics.uci.edu:9092
+KAFKA_BROKER_ID=1
+KAFKA_ZOOKEEPER_CONNECT=kiwi.ics.uci.edu:2181
+
+VOLUME_DIR=/home/kafka-data-$KAFKA_BROKER_ID
+sudo mkdir $VOLUME_DIR
+
+docker run --detach -v $VOLUME_DIR:/tmp/kafka-logs -p 9092:9092 xizzz/kafka \
+	$KAFKA_ADVERTISED_LISTENERS $KAFKA_BROKER_ID $KAFKA_ZOOKEEPER_CONNECT
