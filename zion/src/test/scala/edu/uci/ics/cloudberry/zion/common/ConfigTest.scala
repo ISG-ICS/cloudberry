1a16cff627119044048f55ca7595a7203d1d8efd
haochen07 <haoc13@uci.edu>
2017-02-09 23:33:04 -0800

    Add test cases for parseFrameLengthLimit

--- zion/src/test/scala/edu/uci/ics/cloudberry/zion/common/ConfigTest.scala
+++ zion/src/test/scala/edu/uci/ics/cloudberry/zion/common/ConfigTest.scala
@@ -11,6 +11,17 @@ class ConfigTest extends Specification {
       Config.parseTimePair("43 minute") must_==(43 minutes)
       Config.parseTimePair("84 minutes") must_==(84 minutes)
     }
-
+    "parseFrameLengthLimit" in {
+      Config.parseFrameLengthLimit("20 B") must_==(8 * MemorySize.MB)
+      Config.parseFrameLengthLimit("0 KB") must_==(8 * MemorySize.MB)
+      Config.parseFrameLengthLimit("0.9 KB") must_==(8 * MemorySize.MB)
+      Config.parseFrameLengthLimit("1 KB") must_==(1 * MemorySize.KB)
+      Config.parseFrameLengthLimit("16 MB") must_==(16 * MemorySize.MB)
+      Config.parseFrameLengthLimit("16MB") must_==(16 * MemorySize.MB)
+      Config.parseFrameLengthLimit("1.5 GB") must_==(1.5 * MemorySize.GB)
+      Config.parseFrameLengthLimit("2 GB") must_==(Int.MaxValue)
+      Config.parseFrameLengthLimit("3 GB") must_==(8 * MemorySize.MB)
+      Config.parseFrameLengthLimit("5 TB") must_==(8 * MemorySize.MB)
+    }
   }
 }
