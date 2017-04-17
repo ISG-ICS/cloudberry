aaf0998d05e66c262536e6c1f3865436ad708296
Shengjie Xu <lichkingxsj@gmail.com>
2017-04-17 12:40:54 -0700

    add "M" in the selected count

--- neo/public/javascripts/map/controllers.js
+++ neo/public/javascripts/map/controllers.js
@@ -418,6 +418,7 @@ angular.module('cloudberry.map', ['leaflet-directive', 'cloudberry.common'])
         else{
           d.properties.countText = d.properties.count.toFixed(1);
         }
+        d.properties.countText += "M";
       }
 
       //FIXME: the code in county and city (and probably the state) levels are quite similar. Find a way to combine them.
