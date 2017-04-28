10f1ad5ca5fefc5ba0890caeda8c9c843f20d6dc
hao.chen <haoc13@uci.edu>
2017-04-27 20:38:01 -0700

    [Issue#305] Deregister process can delete record in meta dataset and drop views (#320)
    * finish deleting workflow
    
    * finish deregister without dropView
    
    * deregister finished
    
    * improve memory control
    
    * add test
    
    * kill agent actor
    
    * change map manipulations
    
    * fix in-memory map bug
    
    * fix scala map issue
    
    * DeleteRecord is called inside DropView
    
    * add test
    
    * update test cases
    
    * update test cases
    
    * update test
    
    * add meta test
    
    * fix meta agent dropview

--- zion/src/main/scala/edu/uci/ics/cloudberry/zion/actor/MetaDataAgent.scala
+++ zion/src/main/scala/edu/uci/ics/cloudberry/zion/actor/MetaDataAgent.scala
@@ -1,9 +1,11 @@
 package edu.uci.ics.cloudberry.zion.actor
 
 import akka.actor.Props
+import akka.pattern.ask
 import edu.uci.ics.cloudberry.zion.common.Config
 import edu.uci.ics.cloudberry.zion.model.datastore.{IDataConn, IQLGenerator}
-import edu.uci.ics.cloudberry.zion.model.schema.{Query, Schema, UpsertRecord}
+import edu.uci.ics.cloudberry.zion.model.impl.DataSetInfo
+import edu.uci.ics.cloudberry.zion.model.schema._
 import play.api.libs.json.JsValue
 
 import scala.concurrent.ExecutionContext
@@ -21,6 +23,15 @@ class MetaDataAgent(override val dbName: String,
   override protected def maintenanceWork: Receive = {
     case upsert: UpsertRecord =>
       processUpdate(queryParser.generate(upsert, Map(dbName -> schema)))
+    case delete: DeleteRecord =>
+      processUpdate(queryParser.generate(delete, Map(dbName -> schema)))
+    case drop: DropView =>
+      // Note: parameter SchemaMap is not used in this case.
+      val dropStatement = queryParser.generate(drop, Map())
+      val viewRecordFilter = FilterStatement(DataSetInfo.MetaSchema.fieldMap("name"), None, Relation.matches, Seq(drop.dataset))
+      val delete = DeleteRecord(DataSetInfo.MetaDataDBName, Seq(viewRecordFilter))
+      val deleteStatement = queryParser.generate(delete, Map(dbName -> schema))
+      processUpdate(Seq(dropStatement, deleteStatement))
   }
 }
 
