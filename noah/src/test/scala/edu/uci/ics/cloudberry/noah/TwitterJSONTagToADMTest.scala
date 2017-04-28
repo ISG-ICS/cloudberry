b3dd719ed1ba4bedd62780d13c0f37a9dd01a68a
Jianfeng Jia <jianfeng.jia@gmail.com>
2017-04-28 03:32:55 -0700

    fix path in noah test

--- noah/src/test/scala/edu/uci/ics/cloudberry/noah/TwitterJSONTagToADMTest.scala
+++ noah/src/test/scala/edu/uci/ics/cloudberry/noah/TwitterJSONTagToADMTest.scala
@@ -8,9 +8,9 @@ import edu.uci.ics.cloudberry.util.Profile._
 
 class TwitterJSONTagToADMTest extends FlatSpec with Matchers {
   "USGeoGnosis" should "tag the us json file" in {
-    val shapeMap = Map.apply(StateLevel -> new File("neo/public/data/state.json"),
-                             CountyLevel -> new File("neo/public/data/county.json"),
-                             CityLevel -> new File("neo/public/data/city.json"))
+    val shapeMap = Map.apply(StateLevel -> new File("twittermap/public/data/state.json"),
+                             CountyLevel -> new File("twittermap/public/data/county.json"),
+                             CityLevel -> new File("twittermap/public/data/city.json"))
     val usGeoGnosis = profile("load shapes")(new USGeoGnosis(shapeMap))
     for (ln <- scala.io.Source.fromURL(getClass.getResource("/sample.json")).getLines()) {
       TwitterJSONTagToADM.tagOneTweet(ln, usGeoGnosis)
