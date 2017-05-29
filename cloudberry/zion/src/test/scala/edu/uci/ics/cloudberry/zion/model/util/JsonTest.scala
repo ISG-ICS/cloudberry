d551046c461ffb8ce00545d7c6543187c988936f
Jianfeng Jia <jianfeng.jia@gmail.com>
2017-05-29 07:19:40 -0700

    fix test

--- cloudberry/zion/src/test/scala/edu/uci/ics/cloudberry/zion/model/util/JsonTest.scala
+++ cloudberry/zion/src/test/scala/edu/uci/ics/cloudberry/zion/model/util/JsonTest.scala
@@ -16,56 +16,51 @@ class JsonTest extends Specification {
       val jsArray1 = JsArray(1 to 1000 map (id => Json.obj("k" -> JsString(s"a$id"), "v" -> JsNumber(id))))
       val jsArray2 = JsArray(500 to 1500 map (id => Json.obj("k" -> JsString(s"a$id"), "v" -> JsNumber(id))))
 
-      import edu.uci.ics.cloudberry.util.Profile._
-      profile("using Js") {
-        val updatedArray1 = {
-          jsArray1.value.map { jsValue =>
-            val key = jsValue \ "k"
-            val value = (jsValue \ "v").as[JsNumber].value
-            jsArray2.value.find(r => (r \ "k") == key) match {
-              case Some(v) =>
-                val newVal = JsNumber((v \ "v").as[JsNumber].value + value)
-                jsValue.as[JsObject] ++ Json.obj("v" -> newVal)
-              case None => jsValue
-            }
-          }
-        }
-
-        val onlyInArray2 = {
-          jsArray2.value.filter(r2 => updatedArray1.forall(r1 => (r1 \ "k") != (r2 \ "k")))
-        }
-        val ret = (JsArray(updatedArray1 ++ onlyInArray2))
-      }
-
-
-      profile("using map") {
-        val builder = scala.collection.mutable.Map[Seq[JsValue], Seq[JsValue]]()
-        builder ++= jsArray1.value.map { r =>
-          val key = (r \ "k").get
-          val value = (r \ "v").get
-          Seq(key) -> Seq(value)
-        }
-
-        for (r <- jsArray2.value) {
-          val key = (r \ "k").get
-          val value = (r \ "v").get
-          builder.get(Seq(key)) match {
+      val updatedArray1 = {
+        jsArray1.value.map { jsValue =>
+          val key = jsValue \ "k"
+          val value = (jsValue \ "v").as[JsNumber].value
+          jsArray2.value.find(r => (r \ "k") == key) match {
             case Some(v) =>
-              val newValue = Json.toJson(v.head.as[Int] + value.as[Int])
-              builder += (Seq(key) -> Seq(newValue))
-            case None => builder += (Seq(key) -> Seq(value))
+              val newVal = JsNumber((v \ "v").as[JsNumber].value + value)
+              jsValue.as[JsObject] ++ Json.obj("v" -> newVal)
+            case None => jsValue
           }
         }
-        val ret = (JsArray(builder.toMap.map { case (keys, values) =>
-          val keySeq = keys.map { key =>
-            "k" -> key
-          }
-          val valueSeq = values.map { value =>
-            "v" -> value
-          }
-          JsObject(keySeq ++ valueSeq)
-        }.toSeq))
       }
+
+      val onlyInArray2 = {
+        jsArray2.value.filter(r2 => updatedArray1.forall(r1 => (r1 \ "k") != (r2 \ "k")))
+      }
+      (JsArray(updatedArray1 ++ onlyInArray2))
+
+
+      val builder = scala.collection.mutable.Map[Seq[JsValue], Seq[JsValue]]()
+      builder ++= jsArray1.value.map { r =>
+        val key = (r \ "k").get
+        val value = (r \ "v").get
+        Seq(key) -> Seq(value)
+      }
+
+      for (r <- jsArray2.value) {
+        val key = (r \ "k").get
+        val value = (r \ "v").get
+        builder.get(Seq(key)) match {
+          case Some(v) =>
+            val newValue = Json.toJson(v.head.as[Int] + value.as[Int])
+            builder += (Seq(key) -> Seq(newValue))
+          case None => builder += (Seq(key) -> Seq(value))
+        }
+      }
+      (JsArray(builder.toMap.map { case (keys, values) =>
+        val keySeq = keys.map { key =>
+          "k" -> key
+        }
+        val valueSeq = values.map { value =>
+          "v" -> value
+        }
+        JsObject(keySeq ++ valueSeq)
+      }.toSeq))
       ok
     }
   }
--- examples/twittermap/noah/src/test/scala/edu/uci/ics/cloudberry/noah/TwitterJSONTagToADMTest.scala
+++ examples/twittermap/noah/src/test/scala/edu/uci/ics/cloudberry/noah/TwitterJSONTagToADMTest.scala
@@ -8,9 +8,9 @@ import edu.uci.ics.cloudberry.util.Profile._
 
 class TwitterJSONTagToADMTest extends FlatSpec with Matchers {
   "USGeoGnosis" should "tag the us json file" in {
-    val shapeMap = Map.apply(StateLevel -> new File("twittermap/public/data/state.json"),
-                             CountyLevel -> new File("twittermap/public/data/county.json"),
-                             CityLevel -> new File("twittermap/public/data/city.json"))
+    val shapeMap = Map.apply(StateLevel -> new File("web/public/data/state.json"),
+                             CountyLevel -> new File("web/public/data/county.json"),
+                             CityLevel -> new File("web/public/data/city.json"))
     val usGeoGnosis = profile("load shapes")(new USGeoGnosis(shapeMap))
     for (ln <- scala.io.Source.fromURL(getClass.getResource("/sample.json")).getLines()) {
       TwitterJSONTagToADM.tagOneTweet(ln, usGeoGnosis)
