e7ed1ed1ebd205d7a5a33f46322f0f49a099546d
De-Yu Chen <teyuc@uci.edu>
2017-06-09 09:40:05 -0700

    Add files via upload

--- paraview/webpack.config.js
+++ paraview/webpack.config.js
@@ -1,9 +1,9 @@
-var path = require('path'),
-    webpack = require('webpack'),
-    loaders = require('./node_modules/paraviewweb/config/webpack.loaders.js'),
+var path = require("path"),
+    webpack = require("webpack"),
+    loaders = require("./node_modules/paraviewweb/config/webpack.loaders.js"),
     plugins = [];
-if(process.env.NODE_ENV === 'production') {
-    console.log('==> Production build');
+if(process.env.NODE_ENV === "production") {
+    console.log("==> Production build");
     plugins.push(new webpack.DefinePlugin({
         "process.env": {
             NODE_ENV: JSON.stringify("production"),
@@ -11,23 +11,23 @@ if(process.env.NODE_ENV === 'production') {
     }));
 }
 module.exports = {
-  plugins: plugins,
-  entry: './src/CloudberryDemo.js',
-  output: {
-    path: './dist',
-    filename: 'CloudberryDemo.js',
-  },
-  module: {
+    plugins: plugins,
+    entry: "./src/CloudberryDemo.js",
+    output: {
+        path: "./dist",
+        filename: "CloudberryDemo.js",
+    },
+    module: {
         loaders: [
             { test: require.resolve("./src/CloudberryDemo.js"), loader: "expose?CloudberryDemo" },
         ].concat(loaders),
     },
     resolve: {
         alias: {
-            PVWStyle: path.resolve('./node_modules/paraviewweb/style'),
+            PVWStyle: path.resolve("./node_modules/paraviewweb/style"),
         },
     },
     postcss: [
-        require('autoprefixer')({ browsers: ['last 2 versions'] }),
+        require("autoprefixer")({ browsers: ["last 2 versions"] }),
     ],
 };
