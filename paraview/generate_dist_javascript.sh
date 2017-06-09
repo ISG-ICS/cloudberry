db656bbc6507b2dcd8344865ccb22447ee6f02e0
De-Yu Chen <teyuc@uci.edu>
2017-06-09 13:45:29 -0700

    Create generate_dist_javascript.sh
    Add comments

--- paraview/generate_dist_javascript.sh
+++ paraview/generate_dist_javascript.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-npm install paraviewweb  --save
-npm install kw-web-suite --save-dev
-npm install font-awesome --save
-npm install mout --save
-npm install json-bigint --save
-npm install http-server --save
+npm install paraviewweb  --save       # install the ParaviewWeb npm package
+npm install kw-web-suite --save-dev   # install the development packages required by ParaviewWeb
+npm install font-awesome --save       # a package required by ParaviewWeb but is missing in kw-web-suite
+npm install mout --save               # a package required by ParaviewWeb but is missing in kw-web-suite
+npm install json-bigint --save        # a package required by ParaviewWeb but is missing in kw-web-suite
 
-./node_modules/.bin/webpack
+./node_modules/.bin/webpack           # generate a combine JavaScript file in the 'dist' directory
