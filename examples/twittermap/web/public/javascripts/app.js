7209b21a6b6d1ad98be87062de2fb71bb271776f
Dharini S <dharini.09@gmail.com>
2017-08-26 13:21:22 -0700

    Caching map results with incremental computation

--- examples/twittermap/web/public/javascripts/app.js
+++ examples/twittermap/web/public/javascripts/app.js
@@ -1,4 +1,5 @@
-var app = angular.module('cloudberry', ['cloudberry.map','cloudberry.timeseries','cloudberry.sidebar', 'cloudberry.util','cloudberry.cache']);
+var app = angular.module('cloudberry', ['cloudberry.map','cloudberry.timeseries','cloudberry.sidebar', 'cloudberry.util','cloudberry.cache',
+                          'cloudberry.resultcache']);
 
 app.controller("AppCtrl", function ($scope) {
 });
