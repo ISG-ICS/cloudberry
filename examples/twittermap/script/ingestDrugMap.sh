aa3b9236870c627fad00a34121e1937be909eba8
Taewoo Kim <wangsaeu@yahoo.com>
2018-01-27 14:55:49 -0800

    Apply the current AsterixDB

--- examples/twittermap/script/ingestDrugMap.sh
+++ examples/twittermap/script/ingestDrugMap.sh
@@ -1,35 +1,42 @@
-host=${1:-'http://localhost:19002/aql'}
-nc=${2:-"nc1"}
+host=${1:-'http://localhost:19002/query/service'}
+nc=${2:-"asterix_nc1"}
 # ddl to register the twitter dataset
 cat <<EOF | curl -XPOST --data-binary @- $host
-use dataverse twitter;
-create type typeTweetMoney if not exists as open{
-    create_at : datetime,
-    id: int64,
-    'text': string,
-    retweet_count : int64,
-    lang : string,
-    is_retweet: boolean,
-    user : typeUser,
-    geo_tag: typeGeoTag
-}; 
-create dataset ds_tweet_money(typeTweetMoney) if not exists primary key id;
+use twitter; 
 
-create feed DrugFeed using socket_adapter
-(
-    ("sockets"="$nc:10002"),
-    ("address-type"="nc"),
-    ("type-name"="typeTweetMoney"),
-    ("format"="adm")
-);
-connect feed DrugFeed to dataset ds_tweet_money;
-start feed DrugFeed;
+create type typeTweetMoney if not exists as open{ 
+    create_at : datetime, 
+    id: int64, 
+    text: string, 
+    retweet_count : int64, 
+    lang : string, 
+    is_retweet: boolean, 
+    user : typeUser, 
+    geo_tag: typeGeoTag 
+}; 
+create dataset ds_tweet_money(typeTweetMoney) if not exists primary key id; 
+
+create feed DrugFeed with { 
+    "adapter-name" : "socket_adapter", 
+    "sockets" : "asterix_nc1:10002", 
+    "address-type" : "nc", 
+    "type-name" : "typeTweetMoney", 
+    "format" : "adm", 
+    "upsert-feed" : "false" 
+}; 
+
+connect feed DrugFeed to dataset ds_tweet_money; 
+start feed DrugFeed; 
 
 EOF
 
 echo 'Created drugmap datasets in AsterixDB.'
 #Serve socket feed using local file
-cat ./script/drugmap.adm | ./script/fileFeed.sh $host 10002
+cat ./script/drugmap.adm | ./script/fileFeed.sh 127.0.0.1 10002
 echo 'Ingested drugmap dataset.'
 
-
+cat <<'EOF' | curl -XPOST --data-binary @- $host
+use twitter;  
+stop feed DrugFeed;  
+drop feed DrugFeed;
+EOF
