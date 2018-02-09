fd1f13e96e35dc68f1106b8a83a09399cbd9fc40
Sabbath <teyuc@uci.edu>
2018-02-09 15:34:17 -0800

    Fixed a typing error

--- examples/twittermap/web/public/javascripts/map/controllers.js
+++ examples/twittermap/web/public/javascripts/map/controllers.js
@@ -8,7 +8,7 @@ angular.module('cloudberry.map', ['leaflet-directive', 'cloudberry.common','clou
       var alertDiv = document.getElementsByTagName("alert-bar")[0];
       var div = L.DomUtil.create('div', 'alert alert-warning alert-dismissible')
       div.innerHTML = [
-        '<a hreTweetf="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>',
+        '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>',
         '<strong>Warning! </strong> TwitterMap currently doesn\'t support IE.'
       ].join('');
       div.style.position = 'absolute';
