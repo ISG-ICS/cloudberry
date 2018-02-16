6615d5590bd2a5ec7bd044f5b9647d4c1b6ba6fa
Qiushi <baiqiushi@gmail.com>
2018-02-15 20:18:16 -0800

    Initialize HIVMap before frontend refactoring

---
+++ examples/hivmap/web/public/javascripts/app.js
@@ -0,0 +1,12 @@
+var app = angular.module('cloudberry', ['cloudberry.map','cloudberry.timeseries', 'cloudberry.sidebar',
+                          'cloudberry.util','cloudberry.cache', 'cloudberry.mapresultcache']);
+
+app.controller("AppCtrl", function ($scope, cloudberry, $rootScope) {
+  //When websocket is ready, send the initial default query.
+  $rootScope.$on('ws-ready', function (event, data) {
+    cloudberry.parameters.maptype = 'countmap';
+    cloudberry.parameters.keywords = ['happy'];
+    cloudberry.query(cloudberry.parameters);
+    console.log('Initial query sent.');
+  });
+});
