5348d78aa797da1ad9983e897d8c9f3cbd2a7828
Hao Chen <haoc13@uci.edu>
2018-03-08 11:55:09 -0800

    Make maxTextMessageSize of websocket server configurable (#450)
    * configure max text message size
    
    * add comment

--- examples/twittermap/web/app/controllers/TwitterMapApplication.scala
+++ examples/twittermap/web/app/controllers/TwitterMapApplication.scala
@@ -42,6 +42,7 @@ class TwitterMapApplication @Inject()(val wsClient: WSClient,
   val defaultMapType: String = config.getString("defaultMapType").getOrElse("countmap")
 
   val webSocketFactory = new WebSocketFactory()
+  val maxTextMessageSize: Int = config.getInt("maxTextMessageSize").getOrElse(5* 1024* 1024)
   val clientLogger = Logger("client")
 
   import TwitterMapApplication.DBType
@@ -72,7 +73,7 @@ class TwitterMapApplication @Inject()(val wsClient: WSClient,
 
   def ws = WebSocket.accept[JsValue, JsValue] { request =>
     ActorFlow.actorRef { out =>
-      TwitterMapPigeon.props(webSocketFactory, cloudberryWS, out)
+      TwitterMapPigeon.props(webSocketFactory, cloudberryWS, out, maxTextMessageSize)
     }
   }
 
