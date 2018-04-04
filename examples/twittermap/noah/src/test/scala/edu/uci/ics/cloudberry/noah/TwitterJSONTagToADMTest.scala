d2edbca160322d45733c1a88a9a39f03bc1c6084
Liming Dong <dlm14@mails.tsinghua.edu.cn>
2018-04-04 10:58:20 -0700

    sample.json

--- examples/twittermap/noah/src/test/scala/edu/uci/ics/cloudberry/noah/TwitterJSONTagToADMTest.scala
+++ examples/twittermap/noah/src/test/scala/edu/uci/ics/cloudberry/noah/TwitterJSONTagToADMTest.scala
@@ -8,10 +8,9 @@ import edu.uci.ics.cloudberry.util.Profile._
 
 class TwitterJSONTagToADMTest extends FlatSpec with Matchers {
   "USGeoGnosis" should "tag the us json file" in {
-    val sLoadGeo = new Date()
-    val shapeMap = Map.apply(StateLevel -> new File("web/public/data/state.json"),
-      CountyLevel -> new File("web/public/data/county.json"),
-      CityLevel -> new File("web/public/data/city.json"))
+    val shapeMap = Map.apply(StateLevel -> new File("/root/cloudberry/examples/twittermap/web/public/data/state.json"),
+      CountyLevel -> new File("/root/cloudberry/examples/twittermap/web/public/data/county.json"),
+      CityLevel -> new File("/root/cloudberry/examples/twittermap/web/public/data/city.json"))
     val usGeoGnosis = profile("load shapes")(new USGeoGnosis(shapeMap))
     for (ln <- scala.io.Source.fromURL(getClass.getResource("/sample.json")).getLines()) {
       TwitterJSONTagToADM.tagOneTweet(ln, usGeoGnosis)
