e32ed923b086938c0a0fa22f665ea35063dca026
AC-Unicorn <a894657305@qq.com>
2018-07-06 09:43:10 -0700

    simplify service-multilayer

--- examples/twittermap/web/public/javascripts/common/services-multilayer.js
+++ examples/twittermap/web/public/javascripts/common/services-multilayer.js
@@ -1,10 +1,7 @@
 angular.module('cloudberry.common')
     .service('createLayerService', function(multilayerHeatmap){
         var createLayerService = {
-            polygon: {},
-            countmap: {},
             heatmap: multilayerHeatmap.createLayer,
-            pinmap: {}
         };
         
         return createLayerService;
