45a1593fc57d01b77dcde4294cda9ebe59c6449f
AC-Unicorn <a894657305@qq.com>
2018-07-18 15:53:59 -0700

    further clean

--- examples/twittermap/web/public/javascripts/map/multilayer-manager.js
+++ examples/twittermap/web/public/javascripts/map/multilayer-manager.js
@@ -1,57 +1,57 @@
 angular.module("cloudberry.map")
-  .controller("multiLayerManager", function($timeout, $scope, $rootScope, $window, $http, $compile, cloudberryConfig, cloudberry, leafletData, Cache, createLayerService) {
+  .controller("multiLayerManager", function($scope, $rootScope, cloudberry, createLayerService) {
 
-  cloudberry.parameters.layers = {};
+    cloudberry.parameters.layers = {};
 
-  // This function will watch all maptypeChange events for all layers 
-  $rootScope.$on("maptypeChange", function (event, data) {
-    var layerName = cloudberry.parameters.maptype;
+    // This function will watch all maptypeChange events for all layers 
+    $rootScope.$on("maptypeChange", function (event, data) {
+      var layerName = cloudberry.parameters.maptype;
 
-    for(var key in cloudberry.parameters.layers)
-    {   
-      if(key!==layerName)
-      {       
-        $scope.map.removeLayer(cloudberry.parameters.layers[key].layer);
-        cloudberry.parameters.layers[key].active = 0;
-      }
-      else if(key === layerName && cloudberry.parameters.layers[key].active === 0)
-      {
-        if(typeof cloudberry.parameters.layers[key].activate === "function"){
-          cloudberry.parameters.layers[key].activate();
+      for(var key in cloudberry.parameters.layers)
+      {   
+        if(key!==layerName)
+        {       
+          $scope.map.removeLayer(cloudberry.parameters.layers[key].layer);
+          cloudberry.parameters.layers[key].active = 0;
         }
-        cloudberry.parameters.layers[key].active = 1;
-        $scope.map.addLayer(cloudberry.parameters.layers[key].layer);
-      }
-    }
-
-    cloudberry.query(cloudberry.parameters);
-  });
-  //This function register layer to layer manager 
-  function addLayer(layerID, active, parameters){ 
-    createLayerService[layerID](parameters).then(function(layer){
-      cloudberry.parameters.layers[layerID] = layer;
-      cloudberry.parameters.layers[layerID].init($scope).then(function(){
-        cloudberry.parameters.layers[layerID].active = active;
-        if (cloudberry.parameters.layers[layerID].active){
-          $scope.map.addLayer(cloudberry.parameters.layers[layerID].layer);
+        else if(key === layerName && cloudberry.parameters.layers[key].active === 0)
+        {
+          if(typeof cloudberry.parameters.layers[key].activate === "function"){
+            cloudberry.parameters.layers[key].activate();
+          }
+          cloudberry.parameters.layers[key].active = 1;
+          $scope.map.addLayer(cloudberry.parameters.layers[key].layer);
         }
-      });
+      }
+
+      cloudberry.query(cloudberry.parameters);
     });
-  }         
+    //This function register layer to layer manager 
+    function addLayer(layerID, active, parameters){ 
+      createLayerService[layerID](parameters).then(function(layer){
+        cloudberry.parameters.layers[layerID] = layer;
+        cloudberry.parameters.layers[layerID].init($scope).then(function(){
+          cloudberry.parameters.layers[layerID].active = active;
+          if (cloudberry.parameters.layers[layerID].active){
+            $scope.map.addLayer(cloudberry.parameters.layers[layerID].layer);
+          }
+        });
+      });
+    }         
 
-  $scope.$on("leafletDirectiveMap.zoomend", function() {
-    for (var key in cloudberry.parameters.layers) {
-      if (cloudberry.parameters.layers[key].active && typeof cloudberry.parameters.layers[key].zoom === "function"){
-        cloudberry.parameters.layers[key].zoom();
+    $scope.$on("leafletDirectiveMap.zoomend", function() {
+      for (var key in cloudberry.parameters.layers) {
+        if (cloudberry.parameters.layers[key].active && typeof cloudberry.parameters.layers[key].zoom === "function"){
+          cloudberry.parameters.layers[key].zoom();
+        }
       }
-    }
+    });
+
+    $scope.$on("leafletDirectiveMap.dragend", function() {
+      for (var key in cloudberry.parameters.layers) {
+        if (cloudberry.parameters.layers[key].active && typeof cloudberry.parameters.layers[key].drag === "function"){
+          cloudberry.parameters.layers[key].drag();
+        }
+      }
+    });    
   });
-
-  $scope.$on("leafletDirectiveMap.dragend", function() {
-    for (var key in cloudberry.parameters.layers) {
-      if (cloudberry.parameters.layers[key].active && typeof cloudberry.parameters.layers[key].drag === "function"){
-        cloudberry.parameters.layers[key].drag();
-      }
-    }
-  });    
-});
