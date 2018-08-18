e99d2404e7d2f38004222c3b78c146baec6d1583
AC-Unicorn <a894657305@qq.com>
2018-08-18 00:54:18 -0700

    delete uneccessary files

--- examples/twittermap/web/public/javascripts/common/services-multilayer.js
+++
@@ -1,8 +0,0 @@
-angular.module("cloudberry.common")
-    .service("createLayerService", function(multilayerHeatmap){
-        var createLayerService = {
-            heatmap: multilayerHeatmap.createLayer,
-        };
-        
-        return createLayerService;
-    });
