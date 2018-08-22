5fda064d6c296c12368d813be4e48e37bf22cb04
AC-Unicorn <a894657305@qq.com>
2018-08-22 09:59:59 -0700

    stablize

--- examples/twittermap/web/public/javascripts/map/services-multilayer-countmap.js
+++ examples/twittermap/web/public/javascripts/map/services-multilayer-countmap.js
@@ -526,7 +526,7 @@ angular.module("cloudberry.map")
           }
           // When the query is executed completely, we update the map result cache and time series cache.
           if((cloudberryConfig.querySliceMills > 0 && !angular.isArray(resultSet) &&
-            resultSet['key'] === "done") || cloudberryConfig.querySliceMills <= 0) {
+            resultSet["key"] === "done") || cloudberryConfig.querySliceMills <= 0) {
             MapResultCache.putValues(scope.geoIdsNotInCache, cloudberry.parameters.geoLevel,
               cloudberry.countmapMapResult);
             TimeSeriesCache.putTimeSeriesValues(scope.geoIdsNotInTimeSeriesCache,
