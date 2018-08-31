146c0442ee91c89113bf360dd6d2e2fdaecd2d6e
AC-Unicorn <a894657305@qq.com>
2018-08-31 16:09:53 -0700

    add verbose in proxy server

--- examples/twittermap/script/mini-proxy.py
+++ examples/twittermap/script/mini-proxy.py
@@ -1,24 +1,24 @@
 from flask import Flask,request,Response
 import requests as req
-
+import time
 app = Flask(__name__)
 
 @app.route("/spoof",methods=["POST","GET"])
 def spoof():
     #set user_agent for http request header
+
     user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"
     query_word = request.args.get("query")
     url = "https://twitter.com/i/search/typeahead.json?count=10&filters=true&q="+query_word+"&result_type=topics"
-
+    start = time.time()
     res = req.get(url,headers={"user-agent":user_agent,"referer":"https://twitter.com/"})
-
+    print "Time elapsed for requesting twitter server: ", time.time() - start
     resp = Response(res.content)
-
     resp.headers['Access-Control-Allow-Origin'] = '*'
 
     return resp
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
 
 
