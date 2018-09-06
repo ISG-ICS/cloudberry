878a2e57b684e534fdce0ea7022ac8af19249913
yue leng <yue.leng@foxmail.com>
2018-09-05 18:23:04 -0700

    add jsarray

--- cloudberry/zion/src/test/scala/edu/uci/ics/cloudberry/zion/actor/ViewStatusClientTest.scala
+++ cloudberry/zion/src/test/scala/edu/uci/ics/cloudberry/zion/actor/ViewStatusClientTest.scala
@@ -44,7 +44,7 @@ class ViewStatusClientTest extends TestkitExample with SpecificationLike with Mo
 
       when(mockPlanner.requestViewForQuery(query, sourceInfo, Seq.empty)).thenReturn(true)
 
-      sender.expectMsg(Seq(JsString(s"true")))
+      sender.expectMsg(JsArray(Seq(JsString(s"true"))))
 
       ok
     }
