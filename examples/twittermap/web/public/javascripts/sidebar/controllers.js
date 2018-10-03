25978294afc2aec68146331c5515916b1bbda29d
AC-Unicorn <a894657305@qq.com>
2018-10-03 13:11:56 -0700

    revert

---
+++ examples/twittermap/web/public/javascripts/sidebar/controllers.js
@@ -0,0 +1,301 @@
+angular.module("cloudberry.sidebar", ["cloudberry.common"])
+    .controller("SidebarCtrl", function($scope, $timeout, cloudberry, moduleManager, cloudberryClient, queryUtil, cloudberryConfig) {
+
+        // Flag whether current result is outdated
+        $scope.isHashTagOutdated = true;
+        $scope.isSampleTweetsOutdated = true;
+
+        // Flag whether sidebar tab is open
+        $scope.isHashTagOpen = false;
+        $scope.isSampleTweetsOpen = true;
+
+        $scope.currentTab = "sampletweetTab";
+
+        // Timer for sending query to check whether it can be solved by view
+        $scope.timerCheckQuerySolvableByView = null;
+
+        // queryID used to identify a query, which is sent by timer
+        $scope.nowQueryID = null;
+
+        // A WebSocket that send query to Cloudberry, to check whether it is solvable by view
+        var wsCheckQuerySolvableByView = new WebSocket(cloudberryConfig.checkQuerySolvableByView);
+
+        //Function for the button for close the sidebar, and change the flags
+        $scope.closeRightMenu = function() {
+            document.getElementById("sidebar").style.left = "100%";
+            $scope.showOrHideSidebar(-1);
+        };
+
+        // Function for the button that open the sidebar, and change the flags
+        $scope.openRightMenu = function() {
+            document.getElementById("sidebar").style.left = "76%";
+            $scope.showOrHideSidebar(1);
+        };
+
+        function enableHamburgerButton() {
+            document.getElementById("hamburgerButton").disabled = false;
+        }
+
+        function disableHamburgerButton() {
+            document.getElementById("hamburgerButton").disabled = true;
+        }
+
+        // When receiving messages from websocket, check its queryID and result.
+        // If queryID is matched and result is true, enable the sidebar button and clear timer.
+        wsCheckQuerySolvableByView.onmessage = function(event) {
+            $timeout(function() {
+                var result = JSON.parse(event.data);
+                if (result.id === $scope.nowQueryID && result.value[0]) {
+                    clearInterval($scope.timerCheckQuerySolvableByView);
+                    enableHamburgerButton();
+                }
+            });
+        };
+
+        // Set a timer to sending query to check whether it is solvable, every one second
+        function setTimerToCheckQuery() {
+            var queryToCheck = queryUtil.getHashTagRequest(cloudberry.parameters);
+
+            // Add the queryID for a query in to request
+            queryToCheck["transform"] = {
+                wrap: {
+                    id: cloudberry.parameters.keywords.toString(),
+                    category: "checkQuerySolvableByView"
+                }
+            };
+            $scope.nowQueryID = cloudberry.parameters.keywords.toString();
+            $scope.timerCheckQuerySolvableByView = setInterval(function(){
+                if(wsCheckQuerySolvableByView.readyState === wsCheckQuerySolvableByView.OPEN){
+                    wsCheckQuerySolvableByView.send(JSON.stringify(queryToCheck));
+                }
+            }, 1000);
+        }
+
+        function sendHashTagQuery() {
+            var hashtagRequest = queryUtil.getHashTagRequest(cloudberry.parameters);
+            cloudberryClient.send(hashtagRequest, function(id, resultSet) {
+                cloudberry.commonHashTagResult = resultSet[0];
+            }, "hashtagRequest");
+            $scope.isHashTagOutdated = false;
+        }
+
+        function sendSampleTweetsQuery() {
+            var sampleTweetsRequest = queryUtil.getSampleTweetsRequest(cloudberry.parameters);
+            cloudberryClient.send(sampleTweetsRequest, function(id, resultSet) {
+                cloudberry.commonTweetResult = resultSet[0];
+            }, "sampleTweetsRequest");
+            $scope.isSampleTweetsOutdated = false;
+        }
+
+        function handleSidebarQuery() {
+
+            if ($scope.isHashTagOpen && $scope.isHashTagOutdated) {
+                sendHashTagQuery();
+            }
+
+            if ($scope.isSampleTweetsOpen && $scope.isSampleTweetsOutdated) {
+                sendSampleTweetsQuery();
+            }
+        }
+
+        $scope.showTab = function(tab) {
+
+            if (tab !== $scope.currentTab) {
+                $scope.currentTab = tab;
+            }
+
+            switch (tab) {
+                case "hashtagTab":
+                    $scope.isHashTagOpen = true;
+                    $scope.isSampleTweetsOpen = false;
+                    break;
+                case "sampletweetTab":
+                    $scope.isSampleTweetsOpen = true;
+                    $scope.isHashTagOpen = false;
+                    break;
+                case "about":
+                    $scope.isHashTagOpen = false;
+                    $scope.isSampleTweetsOpen = false;
+                    break;
+                default:
+                    break;
+            }
+
+            handleSidebarQuery();
+        };
+
+        $scope.showOrHideSidebar = function(click) {
+            if (click === -1) {
+                cloudberry.parameters.isSampleTweetsOpen = false;
+                cloudberry.parameters.isHashTagOpen = false;
+            }
+            else {
+                $scope.showTab($scope.currentTab);
+            }
+        };
+
+        function eventHandler(event) {
+            $scope.isHashTagOutdated = true;
+            $scope.isSampleTweetsOutdated = true;
+            handleSidebarQuery();
+        }
+
+        // When the keywords changed, we need to:
+        // 1. clear previous timer 2. close and disable sidebar 3. set a new timer for new keywords
+        function keywordsEventHandler(event) {
+            if($scope.timerCheckQuerySolvableByView) {
+                clearInterval($scope.timerCheckQuerySolvableByView);
+            }
+            setTimerToCheckQuery();
+            $scope.closeRightMenu();
+            disableHamburgerButton();
+            $scope.isHashTagOutdated = true;
+            $scope.isSampleTweetsOutdated = true;
+            handleSidebarQuery();
+        }
+
+        moduleManager.subscribeEvent(moduleManager.EVENT.CHANGE_ZOOM_LEVEL, eventHandler);
+        moduleManager.subscribeEvent(moduleManager.EVENT.CHANGE_REGION_BY_DRAG, eventHandler);
+        moduleManager.subscribeEvent(moduleManager.EVENT.CHANGE_SEARCH_KEYWORD, keywordsEventHandler);
+        moduleManager.subscribeEvent(moduleManager.EVENT.CHANGE_TIME_SERIES_RANGE, eventHandler);
+    })
+    .controller("HashTagCtrl", function ($scope, $window, cloudberry) {
+        $scope.hashTagsList = null;
+        // TODO - get rid of this watch by doing work inside the callback function in sendHashTagQuery()
+        $scope.$watch(
+            function () {
+                return cloudberry.commonHashTagResult;
+            },
+            function (newResult) {
+                $scope.hashTagsList = newResult;
+            }
+        );
+    })
+    .directive("hashtag", function () {
+        return {
+            restrict: "E",
+            controller: "HashTagCtrl",
+            template: [
+                '<table class="table" id="hashcount">',
+                '<thead>',
+                '<tr ng-repeat="r in hashTagsList | orderBy:\'-count\'"><td># {{r.tag}}</td><br/><td>{{r.count}}</td></tr>',
+                '</thead>',
+                '</table>'
+            ].join('')
+        };
+    })
+    .controller("TweetCtrl", function ($scope, $window, $http, cloudberry) {
+        $scope.results = {};
+
+        function drawTweets(message) {
+            $('#tweet').html("");
+            if (message) {
+                $.each(message, function (i, d) {
+                    var url = "https://api.twitter.com/1/statuses/oembed.json?callback=JSON_CALLBACK&id=" + d.id;
+                    $http.jsonp(url).success(function (data) {
+                        $("#tweet").append(data.html);
+                    });
+                });
+            }
+        }
+
+        // TODO - get rid of this watch by doing work inside the callback function in sendSampleTweetsQuery()
+        $scope.$watch(
+            function () {
+                return cloudberry.commonTweetResult;
+            },
+            function (newResult) {
+                $scope.results = newResult;
+                drawTweets($scope.results);
+            }
+        );
+    })
+    .directive("tweet", function () {
+        return {
+            restrict: "E",
+            controller: "TweetCtrl"
+        };
+    })
+
+    .controller("choosemap", function ($scope, $window, cloudberry, $rootScope, moduleManager) {
+
+        $scope.result = null;
+        cloudberry.parameters.maptype = config.defaultMapType;
+
+        var icon1 = document.getElementById("img1");
+        var icon2 = document.getElementById("img2");
+        var icon3 = document.getElementById("img3");
+
+        switch (cloudberry.parameters.maptype){
+            case "countmap":
+                icon1.src = "/assets/images/aggregation_map.png";
+                icon2.src = "/assets/images/heat_map_no_border.png";
+                icon3.src = "/assets/images/point_map_no_border.png";
+                break;
+
+            case "heatmap":
+                icon1.src = "/assets/images/aggregation_map_no_border.png";
+                icon2.src = "/assets/images/heat_map.png";
+                icon3.src = "/assets/images/point_map_no_border.png";
+                break;
+
+            case "pinmap":
+                icon1.src = "/assets/images/aggregation_map_no_border.png";
+                icon2.src = "/assets/images/heat_map_no_border.png";
+                icon3.src = "/assets/images/point_map.png";
+                break;
+
+            default:
+                break;
+        }
+
+        icon1.addEventListener("click", function () {
+
+            if (cloudberry.parameters.maptype !== "countmap") {
+                var premaptype = cloudberry.parameters.maptype;
+                cloudberry.parameters.maptype = "countmap";
+                icon1.src = "/assets/images/aggregation_map.png";
+                icon2.src = "/assets/images/heat_map_no_border.png";
+                icon3.src = "/assets/images/point_map_no_border.png";
+                moduleManager.publishEvent(moduleManager.EVENT.CHANGE_MAP_TYPE,
+                    {previousMapType: premaptype, currentMapType: cloudberry.parameters.maptype});
+            }
+
+        });
+
+        icon2.addEventListener("click", function () {
+
+            if (cloudberry.parameters.maptype !== "heatmap") {
+                var premaptype = cloudberry.parameters.maptype;
+                cloudberry.parameters.maptype = "heatmap";
+                icon1.src = "/assets/images/aggregation_map_no_border.png";
+                icon2.src = "/assets/images/heat_map.png";
+                icon3.src = "/assets/images/point_map_no_border.png";
+                moduleManager.publishEvent(moduleManager.EVENT.CHANGE_MAP_TYPE,
+                    {previousMapType: premaptype, currentMapType: cloudberry.parameters.maptype});
+            }
+
+        });
+
+        icon3.addEventListener("click", function () {
+
+            if (cloudberry.parameters.maptype !== "pinmap") {
+                var premaptype = cloudberry.parameters.maptype;
+                cloudberry.parameters.maptype = "pinmap";
+                icon1.src = "/assets/images/aggregation_map_no_border.png";
+                icon2.src = "/assets/images/heat_map_no_border.png";
+                icon3.src = "/assets/images/point_map.png";
+                moduleManager.publishEvent(moduleManager.EVENT.CHANGE_MAP_TYPE,
+                    {previousMapType: premaptype, currentMapType: cloudberry.parameters.maptype});
+            }
+
+        });
+
+    })
+
+    .directive("mapchoose", function () {
+        return {
+            restrict: "E",
+            controller: "choosemap"
+        };
+    });
