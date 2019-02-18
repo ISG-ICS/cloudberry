08aa177fff2fa12db9e1c897361fa78d0d072bef
baiqiushi <baiqiushi@gmail.com>
2019-02-18 11:11:36 -0800

    remove comments in streamFeed.sh

--- examples/twittermap/script/streamFeed.sh
+++ examples/twittermap/script/streamFeed.sh
@@ -8,6 +8,5 @@ Your Consumer Secret
 Your Access token
 -ts
 Your Acces Secret Token
-# southwestern northeastern corners of the U.S.
--loc -173.847656,17.644022,-65.390625,70.377854 \  
--u 127.0.0.1 -p 10001 -w 0 -b 50"  # hostname and port of AsterixDB Feed
+-loc -173.847656,17.644022,-65.390625,70.377854
+-u 127.0.0.1 -p 10001 -w 0 -b 50"
