08dfbdce401891f921158f967fb15c5e91b12cfc
baiqiushi <baiqiushi@gmail.com>
2019-02-28 15:46:37 -0800

    refactor cloudberry-client service

--- examples/twittermap/web/public/javascripts/common/cloudberry-client.js
+++ examples/twittermap/web/public/javascripts/common/cloudberry-client.js
@@ -19,91 +19,87 @@ angular.module("cloudberry.common")
 
     var ws;
 
-    var cloudberryClient = {
+    var queryToResultHandlerMap = {};
 
-      queryToResultHandlerMap: {},
+    /**
+     * send a query to Cloudberry
+     *
+     * @param query JSON object, query JSON for Cloudberry
+     * @param resultHandler function, callback function(id, resultSet, timeInterval),
+     *          if your query's slice option is ON, resultHandler can be called several times.
+     * @param category String, not null, category of this query
+     * @param id String, identification for this query, if null,
+     *                   always use the same resultHandler for this category with id assigned "defaultID"
+     *           * NOTE: resultHandler for one unique id can be registered only once at the first time
+     *                 you call this send function.
+     * @returns {boolean}
+     */
+    this.send = function(query, resultHandler, category, id) {
 
-      /**
-       * send a query to Cloudberry
-       *
-       * @param query JSON object, query JSON for Cloudberry
-       * @param resultHandler function, callback function(id, resultSet, timeInterval),
-       *          if your query's slice option is ON, resultHandler can be called several times.
-       * @param category String, not null, category of this query
-       * @param id String, identification for this query, if null,
-       *                   always use the same resultHandler for this category with id assigned "defaultID"
-       *           * NOTE: resultHandler for one unique id can be registered only once at the first time
-       *                 you call this send function.
-       * @returns {boolean}
-       */
-      send(query, resultHandler, category, id) {
+      var queryCategory = category;
+      var queryID = "defaultID";
 
-        var queryCategory = category;
-        var queryID = "defaultID";
-
-        if (ws.readyState !== ws.OPEN) {
-          return false;
-        }
-
-        if (typeof queryCategory === "undefined" || queryCategory === null) {
-          return false;
-        }
-
-        // This query has unique resultHandler for this queryID
-        if(typeof id !== "undefined" && id !== null) {
-          queryID = id;
-        }
-
-        // The first time registering queryCategory
-        if (!(queryCategory in cloudberryClient.queryToResultHandlerMap)) {
-          cloudberryClient.queryToResultHandlerMap[queryCategory] = {};
-        }
-
-        // The first time registering queryID inside queryCategory
-        if (!(queryID in cloudberryClient.queryToResultHandlerMap[queryCategory])) {
-          cloudberryClient.queryToResultHandlerMap[queryCategory][queryID] = resultHandler;
-        }
-
-        // Add "transform" attribute to the query JSON
-        query["transform"] = {
-          wrap: {
-            id: queryID,
-            category: queryCategory
-          }
-        };
-
-        var request = JSON.stringify(query);
-
-        ws.send(request);
-
-        return true;
-      },
-
-      connectWS(url) {
-        console.log("[cloudberry-client] connecting to " + url);
-
-        var deferred = new $.Deferred();
-        var lws = new WebSocket(url);
-
-        lws.onopen = function () {
-          console.log("[cloudberry-client] ws " + url + " connected...");
-          deferred.resolve(lws);
-        };
-
-        lws.onerror = function (err) {
-          console.log(err);
-          lws.close();
-        };
-
-        lws.onclose = function (e) {
-          setTimeout(function () {
-            cloudberryClient.connectWS(url);
-          }, 500);
-        };
-
-        return deferred.promise();
+      if (ws.readyState !== ws.OPEN) {
+        return false;
       }
 
+      if (typeof queryCategory === "undefined" || queryCategory === null) {
+        return false;
+      }
+
+      // This query has unique resultHandler for this queryID
+      if(typeof id !== "undefined" && id !== null) {
+        queryID = id;
+      }
+
+      // The first time registering queryCategory
+      if (!(queryCategory in cloudberryClient.queryToResultHandlerMap)) {
+        cloudberryClient.queryToResultHandlerMap[queryCategory] = {};
+      }
+
+      // The first time registering queryID inside queryCategory
+      if (!(queryID in cloudberryClient.queryToResultHandlerMap[queryCategory])) {
+        cloudberryClient.queryToResultHandlerMap[queryCategory][queryID] = resultHandler;
+      }
+
+      // Add "transform" attribute to the query JSON
+      query["transform"] = {
+        wrap: {
+          id: queryID,
+          category: queryCategory
+        }
+      };
+
+      var request = JSON.stringify(query);
+
+      ws.send(request);
+
+      return true;
+    };
+
+    this.connectWS = function(url) {
+      console.log("[cloudberry-client] connecting to " + url);
+
+      var deferred = new $.Deferred();
+      var lws = new WebSocket(url);
+
+      lws.onopen = function () {
+        console.log("[cloudberry-client] ws " + url + " connected...");
+        deferred.resolve(lws);
+      };
+
+      lws.onerror = function (err) {
+        console.log(err);
+        lws.close();
+      };
+
+      lws.onclose = function (e) {
+        setTimeout(function () {
+          cloudberryClient.connectWS(url);
+        }, 500);
+      };
+
+      return deferred.promise();
     };
 
     var wsConnection = cloudberryClient.connectWS(cloudberryConfig.ws);
@@ -129,7 +125,5 @@ angular.module("cloudberry.common")
         });
       };
     });
-
-    return cloudberryClient;
 
   });
