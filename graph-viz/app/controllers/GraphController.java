05cc31167b4a23896e429d13868058ad4ab696d5
Yubo Ding <casselyou@gmail.com>
2019-05-31 17:31:42 -0700

    point implementation and remove duplicate edges

--- graph-viz/app/controllers/GraphController.java
+++ graph-viz/app/controllers/GraphController.java
@@ -8,7 +8,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 import java.util.HashSet;
-
+import java.util.Set;
 import java.sql.PreparedStatement;
 
 import play.libs.Json;
@@ -32,7 +32,7 @@ public class GraphController extends Controller {
      */
 
 
-    public Result index(String query) {
+    public Result index(String query, double lowerLongitude, double upperLongitude, double lowerLatitude, double upperLatitude) {
 
         long startIndex = System.currentTimeMillis();
 
@@ -42,40 +42,74 @@ public class GraphController extends Controller {
             Connection conn = DriverManager
                     .getConnection("jdbc:postgresql://localhost:5432/graphtweet",
                             "graphuser", "graphuser");
-            Statement statement = conn.createStatement();
-            ResultSet resultSet = statement.executeQuery("select " +
+            String searchQuery = "select " +
                     "from_longitude, from_latitude, " +
                     "to_longitude, to_latitude " +
                     "from replytweets where " +
                     "to_tsvector('english', from_text) " +
-                    "@@ to_tsquery('"+query+"') or " +
+                    "@@ to_tsquery( ? ) or " +
                     "to_tsvector('english', to_text) " +
-                    "@@ to_tsquery('"+query+"');");
+                    "@@ to_tsquery( ? ) and ((from_longitude >= ? AND from_longitude <= ? AND from_latitude >= ? AND from_latitude <= ?) OR" +
+                    " (to_longitude >= ? AND to_longitude <= ? AND to_latitude >= ? AND to_latitude <= ?));";
+            
+            PreparedStatement state = conn.prepareStatement(searchQuery);
+            state.setString(1, query);
+            state.setString(2, query);
+            state.setDouble(3, lowerLongitude);
+            state.setDouble(4, upperLongitude);
+            state.setDouble(5,lowerLatitude);
+            state.setDouble(6, upperLatitude);
+            state.setDouble(7, lowerLongitude);
+            state.setDouble(8, upperLongitude);
+            state.setDouble(9,lowerLatitude);
+            state.setDouble(10, upperLatitude);
+            ResultSet resultSet = state.executeQuery();
 
+            
             long startParse = System.currentTimeMillis();
 
-
+            //HashSet for deduplicating same edge
+            Set<Edge> edges = new HashSet<>();
+            int numOfSameEdge = 0;
             while (resultSet.next()) {
+                   
                 ObjectNode reply = Json.newObject();
                 ArrayNode fromCoordinate = Json.newArray();
-                fromCoordinate.add(resultSet.getFloat("from_longitude"));
-                fromCoordinate.add(resultSet.getFloat("from_latitude"));
-                reply.set("source", fromCoordinate);
-                ArrayNode toCoordinate = Json.newArray();
-                toCoordinate.add(resultSet.getFloat("to_longitude"));
-                toCoordinate.add(resultSet.getFloat("to_latitude"));
-                reply.set("target", toCoordinate);
-                replies.add(reply);
+
+                //Get all the coords
+                double fromLongtitude = resultSet.getDouble("from_longitude");
+                double fromLatitude = resultSet.getDouble("from_latitude");
+                double toLongtitude = resultSet.getDouble("to_longitude");
+                double toLatitude = resultSet.getFloat("to_latitude");
+
+                //Create Edge and add it if not seen
+                Edge currentEdge = new Edge(fromLatitude, fromLongtitude, toLatitude, toLongtitude);
+                if (edges.contains(currentEdge)){
+                    numOfSameEdge+=1;
+                } 
+                else {
+                    edges.add(currentEdge);
+
+                    fromCoordinate.add(fromLongtitude);
+                    fromCoordinate.add(fromLatitude);
+                    reply.set("source", fromCoordinate);
+                    ArrayNode toCoordinate = Json.newArray();
+                    toCoordinate.add(toLongtitude);
+                    toCoordinate.add(toLatitude);
+                    reply.set("target", toCoordinate);
+                    replies.add(reply);
+                }
             }
             long endIndex = System.currentTimeMillis();
 
             resultSet.close();
-            statement.close();
+            state.close();
             conn.close();
 
             System.out.print("Total time in parsing data from Json is " + (endIndex - startParse) + " ms\n" );
             System.out.print("Total time in executing query in database is " + (startParse - startIndex) + " ms\n" );
             System.out.print("Total time in running index() is " + (endIndex - startIndex) + " ms\n" );
+            System.out.print("Total number of Same Edge that are not added to Json is "+String.valueOf(numOfSameEdge));
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
