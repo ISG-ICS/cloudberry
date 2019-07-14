f739298c7d295cf3dff1575553535de6b8de6c9c
ridiculousJoe <11611026@mail.sustech.edu.cn>
2019-07-13 21:47:17 -0700

    update json_to_csv.py

--- graph-viz/data/json_to_csv.py
+++ graph-viz/data/json_to_csv.py
@@ -1,10 +1,11 @@
 import json
 import csv
 
+# for float comparison
 eps = 1e-5
 
+# status = 1, 2, 3, 4
 status = 1
-# status = 1,2,3,4
 
 
 def detail_error_detect(data, label, i):
@@ -29,12 +30,12 @@ def key_error_detect(data, i):
         status = 4
 
 
-def create_list_from_json(jsonfile):
+def create_list_from_json(json_file):
 
-    with open(jsonfile) as f:
+    with open(json_file) as f:
         data = json.load(f)
 
-    all_list = []
+    all_data = []
     for i in range(len(data)):
         key_error_detect(data, i)
         x_from_box = (data[i]['ds_tweet_reply_graph']['from_bounding_box'][0][0] + data[i]['ds_tweet_reply_graph']['from_bounding_box'][1][0]) / 2
@@ -43,7 +44,7 @@ def create_list_from_json(jsonfile):
         y_to_box = (data[i]['ds_tweet_reply_graph']['to_bounding_box'][0][0] + data[i]['ds_tweet_reply_graph']['to_bounding_box'][1][0]) / 2
         source = []
         target = []
-        # source and target are the same, don't add the record
+        # if source and target are the same, don't add the record
         if status == 1:
             source = [data[i]['ds_tweet_reply_graph']['from_coordinate'][0], data[i]['ds_tweet_reply_graph']['from_coordinate'][1]]
             target = [data[i]['ds_tweet_reply_graph']['to_coordinate'][0], data[i]['ds_tweet_reply_graph']['to_coordinate'][1]]
@@ -60,12 +61,14 @@ def create_list_from_json(jsonfile):
                 and abs(source[1] - target[1]) <= eps:
             continue
 
+        # ignore "\n" and "|" in texts
         data[i]['ds_tweet_reply_graph']['from_text'] = data[i]['ds_tweet_reply_graph']['from_text'].replace("\n", " ")
         data[i]['ds_tweet_reply_graph']['from_text'] = data[i]['ds_tweet_reply_graph']['from_text'].replace("|", "")
         data[i]['ds_tweet_reply_graph']['to_text'] = data[i]['ds_tweet_reply_graph']['to_text'].replace("\n", " ")
         data[i]['ds_tweet_reply_graph']['to_text'] = data[i]['ds_tweet_reply_graph']['to_text'].replace("|", "")
 
-        data_list = [data[i]['ds_tweet_reply_graph']['tweet_from'],
+        # form a line of data
+        data_line = [data[i]['ds_tweet_reply_graph']['tweet_from'],
                      data[i]['ds_tweet_reply_graph']['from_user'],
                      data[i]['ds_tweet_reply_graph']['from_create_at'],
                      data[i]['ds_tweet_reply_graph']['from_text'],
@@ -78,18 +81,18 @@ def create_list_from_json(jsonfile):
                      target[0],
                      target[1]]
 
-        all_list.append(data_list)
+        all_data.append(data_line)
 
-    return all_list
+    return all_data
 
 
 def write_csv(path):
-    rows = create_list_from_json(path)  # create the row to be added to csv for each file (json-file)
-    with open('user_id.csv', 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile, delimiter='|', quoting=csv.QUOTE_MINIMAL)
+    rows = create_list_from_json(path)
+    with open('user_id.csv', 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file, delimiter='|', quoting=csv.QUOTE_MINIMAL)
         for row in rows:
             writer.writerow(row)
-    csvfile.close()
+    csv_file.close()
 
 
 if __name__ == '__main__':
