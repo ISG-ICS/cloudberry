02b18c5cae2625202be563eea979dba26fa8ce60
SustechJoy <11612917@mail.sustech.edu.cn>
2019-07-13 21:53:15 -0700

    finish json_to_csv.py

--- graph-viz/data/json_to_csv.py
+++ graph-viz/data/json_to_csv.py
@@ -10,7 +10,7 @@ status = 1
 
 def detail_error_detect(data, label, i):
     try:
-        expected_data = data[i]['ds_tweet_reply_graph'][label]
+        _ = data[i]['ds_tweet_reply_graph'][label]
         return True
     except KeyError:
         return False
@@ -31,29 +31,36 @@ def key_error_detect(data, i):
 
 
 def create_list_from_json(json_file):
-
     with open(json_file) as f:
         data = json.load(f)
 
     all_data = []
     for i in range(len(data)):
         key_error_detect(data, i)
-        x_from_box = (data[i]['ds_tweet_reply_graph']['from_bounding_box'][0][0] + data[i]['ds_tweet_reply_graph']['from_bounding_box'][1][0]) / 2
-        x_to_box = (data[i]['ds_tweet_reply_graph']['to_bounding_box'][0][0] + data[i]['ds_tweet_reply_graph']['to_bounding_box'][1][0]) / 2
-        y_from_box = (data[i]['ds_tweet_reply_graph']['from_bounding_box'][0][1] + data[i]['ds_tweet_reply_graph']['from_bounding_box'][1][1]) / 2
-        y_to_box = (data[i]['ds_tweet_reply_graph']['to_bounding_box'][0][0] + data[i]['ds_tweet_reply_graph']['to_bounding_box'][1][0]) / 2
+        x_from_box = (data[i]['ds_tweet_reply_graph']['from_bounding_box'][0][0] +
+                      data[i]['ds_tweet_reply_graph']['from_bounding_box'][1][0]) / 2
+        x_to_box = (data[i]['ds_tweet_reply_graph']['to_bounding_box'][0][0] +
+                    data[i]['ds_tweet_reply_graph']['to_bounding_box'][1][0]) / 2
+        y_from_box = (data[i]['ds_tweet_reply_graph']['from_bounding_box'][0][1] +
+                      data[i]['ds_tweet_reply_graph']['from_bounding_box'][1][1]) / 2
+        y_to_box = (data[i]['ds_tweet_reply_graph']['to_bounding_box'][0][1] +
+                    data[i]['ds_tweet_reply_graph']['to_bounding_box'][1][1]) / 2
         source = []
         target = []
         # if source and target are the same, don't add the record
         if status == 1:
-            source = [data[i]['ds_tweet_reply_graph']['from_coordinate'][0], data[i]['ds_tweet_reply_graph']['from_coordinate'][1]]
-            target = [data[i]['ds_tweet_reply_graph']['to_coordinate'][0], data[i]['ds_tweet_reply_graph']['to_coordinate'][1]]
+            source = [data[i]['ds_tweet_reply_graph']['from_coordinate'][0],
+                      data[i]['ds_tweet_reply_graph']['from_coordinate'][1]]
+            target = [data[i]['ds_tweet_reply_graph']['to_coordinate'][0],
+                      data[i]['ds_tweet_reply_graph']['to_coordinate'][1]]
         elif status == 2:
-            source = [data[i]['ds_tweet_reply_graph']['from_coordinate'][0], data[i]['ds_tweet_reply_graph']['from_coordinate'][1]]
+            source = [data[i]['ds_tweet_reply_graph']['from_coordinate'][0],
+                      data[i]['ds_tweet_reply_graph']['from_coordinate'][1]]
             target = [x_to_box, y_to_box]
         elif status == 3:
             source = [x_from_box, y_from_box]
-            target = [data[i]['ds_tweet_reply_graph']['to_coordinate'][0], data[i]['ds_tweet_reply_graph']['to_coordinate'][1]]
+            target = [data[i]['ds_tweet_reply_graph']['to_coordinate'][0],
+                      data[i]['ds_tweet_reply_graph']['to_coordinate'][1]]
         elif status == 4:
             source = [x_from_box, y_from_box]
             target = [x_to_box, y_to_box]
@@ -89,10 +96,11 @@ def create_list_from_json(json_file):
 def write_csv(path):
     rows = create_list_from_json(path)
     with open('user_id.csv', 'w', newline='') as csv_file:
+        # csv.QUOTE_MINIMAL means only when required.
+        # csv.QUOTE_NONE means that quotes are never placed around fields.
         writer = csv.writer(csv_file, delimiter='|', quoting=csv.QUOTE_MINIMAL)
         for row in rows:
             writer.writerow(row)
-    csv_file.close()
 
 
 if __name__ == '__main__':
