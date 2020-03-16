b3f7a2525e424b48dcfb43dd48b1f56aa074e17c
heyhalcyon <46017717+heyhalcyon@users.noreply.github.com>
2020-03-16 11:55:30 -0700

    Grafana middleware (#698)
    * Sandbox: Grafana middleware

---
+++ sandbox/grafana-middleware-2019/middlewares/QueryOptMiddleware.py
@@ -0,0 +1,111 @@
+'''
+Query-opt Middleware
+    A middleware that optimizes query using random sampling when data is large
+'''
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from optparse import OptionParser
+import urllib.parse as up
+import requests
+import gzip
+import json
+import math
+from utils import QueryInfo as qi
+import timeit
+
+class RequestHandler(BaseHTTPRequestHandler):
+    
+    def do_GET(self):
+        doGet_start = timeit.default_timer()
+        
+        request_path = self.path
+        
+        print("\n----- Request Start ----->\n")
+        print("Request path:", request_path)
+        print("<----- Request End -----\n")
+        
+        influx_url = "http://localhost:8086"+request_path
+
+## sent query to influxdb
+
+        urlquery = up.urlparse(influx_url).query
+        tuple_list = up.parse_qsl(urlquery)
+        query_string = tuple_list[-1][1]
+
+## COUNT THE DATA by sending a query to infludb
+        q = request_path.split('+')
+        q[1] = ('count%28%2A%29')
+        count = '+'.join(q)
+        count_url = "http://localhost:8086"+count
+        count_start = timeit.default_timer()
+        count_query = requests.get(count_url)
+        count_end = timeit.default_timer()
+        print("count query time:", count_end - count_start)
+        jdict = json.loads(count_query.content)
+        count = jdict["results"][0]["series"][0]["values"][0][1]
+
+## Sample the dataset with more than 2k points
+        max_point = 2000
+        if count <= max_point:
+            print("DIDN'T MODIFY THE QUERY")
+ 
+        else:
+            query_info = qi.QueryInfo(query_string)
+            lowerlimit = query_info.get_time_range()[0]
+            upperlimit = query_info.get_time_range()[1]
+            groupsize = math.floor((upperlimit-lowerlimit)/max_point)
+
+            new_query = query_info.change_to_sample(max_point)
+            
+            new_tuple =("q",new_query)
+            tuple_list[-1] = new_tuple
+
+            parturl = up.urlencode(tuple_list)
+            new_url = "http://localhost:8086/query?"+parturl
+            influx_url = new_url
+            
+## Get sample data from influxdb
+        start = timeit.default_timer()
+        influx_resp = requests.get(influx_url)
+        ## execute the query in influxdb
+        stop = timeit.default_timer()
+        print('Query Time: ', stop - start) 
+ 
+        
+        json_dict = json.loads(influx_resp.content)
+        print("####################")
+
+        data = json_dict["results"][0]["series"][0]["values"]        
+
+## Forward infludb hearder respone to frontend
+        self.send_response(200)
+        for key, value in influx_resp.headers.items():
+            self.send_header(key,value)
+        self.end_headers()
+
+## Compress the file and sent to frontend
+        res = influx_resp.content
+        gres = gzip.compress(res)
+        self.wfile.write(gres)               
+            
+        doGet_end = timeit.default_timer()
+        print("Total time in middleware: ",doGet_end-doGet_start)
+        
+    do_PUT = do_POST
+    do_DELETE = do_GET
+
+        
+def main():
+    port = 8080
+    print('Listening on localhost:%s' % port)
+    server = HTTPServer(('', port), RequestHandler)
+    server.serve_forever()
+
+        
+if __name__ == "__main__":
+    parser = OptionParser()
+    parser.usage = ("Creates an http-server that will echo out any GET or POST parameters\n"
+                    "Run:\n\n"
+                    "   reflect")
+    (options, args) = parser.parse_args()
+    
+    main()
---
+++ sandbox/grafana-middleware-2019/utils/QueryInfo.py
@@ -0,0 +1,84 @@
+'''
+A simple parser for query
+    NOTE: The current solution does not work well on all the query.
+    May use flex&bison to make a better parser.
+'''
+class QueryInfo:
+    def __init__(self, query:str):
+##  query:  SELECT mean("degrees")
+##          FROM "h2o_temperature"
+##          WHERE time >= 1546372062774ms and time <= 1547689662774ms
+##          GROUP BY time(1h) fill(null)
+        self.qs = query  #query string
+        self.qlist = query.lower().split()
+        self.time_range = [0,0]
+        self.se = []  #select
+        self.wh = []  #where
+        self.fr = []  #from
+        self.gb = []  #group by
+        self.other_info = [] 
+        self._parse()
+
+    def _parse(self):
+        i = 0
+        qlist_len = len(self.qlist)
+        if self.qlist[i] == "select":
+            i+=1
+            while(self.qlist[i] != "from"):
+                self.se.append(self.qlist[i])
+                i+=1
+        if self.qlist[i] == "from":
+            i+=1
+            while(self.qlist[i] != "where"):
+                self.fr.append(self.qlist[i])
+                i+=1
+        if self.qlist[i] == "where":
+            i+=1
+            while(i < qlist_len and self.qlist[i] != "group"):
+                self.wh.append(self.qlist[i])
+                i+=1
+        if i < qlist_len and self.qlist[i] == "group":
+            i += 2 # group by
+            while(i < qlist_len):
+                self.gb.append(self.qlist[i])
+                i+=1
+        
+
+    def get_time_range(self):
+        i = self.qlist.index('>=')
+        time_str = self.qlist[i+1]
+        self.time_range[0] = int(time_str[:len(time_str)-2])
+        i = self.qlist.index('<=')
+        time_str = self.qlist[i+1]
+        self.time_range[1] = int(time_str[:len(time_str)-2])
+
+        return self.time_range
+
+    def add_group_by(self, time:int):
+        self._take_mean()
+        self.qs+= " GROUP BY time(" + str(time) + "ms)"
+        return self.qs
+
+    def change_to_sample(self, num:int):
+        self.se[0] = "sample({},{})".format(self.se[0],num)
+        self.qs = "SELECT " + self.se[0] + " FROM " + self.fr[0] + " WHERE " + ' '.join(self.wh)
+        return self.qs
+    
+    def _take_mean(self):
+        if "mean" in self.qlist[1]:
+            return
+        else:
+            self.qlist[1] = "mean(" + self.qlist[1] + ")"
+            self.qs = " ".join(self.qlist)
+        
+
+            
+
+if __name__ == '__main__':
+    print("____test____")
+    qs = 'SELECT "degrees" FROM "h2o_temperature" WHERE time >= 1546372062774ms and time <= 1547689662774ms'
+    test = QueryInfo(qs)
+    test.change_to_sample(2000)
+    tr = test.get_time_range()
+    print(test.qs)
+    
