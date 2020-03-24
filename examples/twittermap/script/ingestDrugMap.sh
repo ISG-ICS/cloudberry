2253169c8a6d1cdd9e19910d01efc2d2c622a663
Ian Maxon <ian@maxons.email>
2020-03-24 16:52:35 -0700

    Update AsterixDB API usage

--- examples/twittermap/script/ingestDrugMap.sh
+++ examples/twittermap/script/ingestDrugMap.sh
@@ -1,8 +1,7 @@
 host=${1:-'http://localhost:19002/query/service'}
 nc=${2:-"asterix_nc1"}
 # ddl to register the twitter dataset
-cat <<EOF | curl -XPOST --data-binary @- $host
-use twitter; 
+curl -XPOST $host --data-urlencode "statement=use twitter; 
 
 create type typeTweetMoney if not exists as open{ 
     create_at : datetime, 
@@ -17,26 +16,22 @@ create type typeTweetMoney if not exists as open{
 create dataset ds_tweet_money(typeTweetMoney) if not exists primary key id; 
 
 create feed DrugFeed with { 
-    "adapter-name" : "socket_adapter", 
-    "sockets" : "asterix_nc1:10002", 
-    "address-type" : "nc", 
-    "type-name" : "typeTweetMoney", 
-    "format" : "adm", 
-    "upsert-feed" : "false" 
+    \"adapter-name\" : \"socket_adapter\", 
+    \"sockets\" : \"asterix_nc1:10002\", 
+    \"address-type\" : \"nc\", 
+    \"type-name\" : \"typeTweetMoney\", 
+    \"format\" : \"adm\", 
+    \"upsert-feed\" : \"false\" 
 }; 
 
 connect feed DrugFeed to dataset ds_tweet_money; 
-start feed DrugFeed; 
-
-EOF
+start feed DrugFeed;"
 
 echo 'Created drugmap datasets in AsterixDB.'
 #Serve socket feed using local file
 cat ./script/drugmap.adm | ./script/fileFeed.sh 127.0.0.1 10002
 echo 'Ingested drugmap dataset.'
 
-cat <<'EOF' | curl -XPOST --data-binary @- $host
-use twitter;  
+curl -XPOST $host --data-urlencode "statement=use twitter;  
 stop feed DrugFeed;  
-drop feed DrugFeed;
-EOF
+drop feed DrugFeed;"
