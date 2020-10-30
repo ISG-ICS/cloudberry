35f7d3e3ad82c8f8c09034ef36cfe90091a10029
baiqiushi <baiqiushi@gmail.com>
2020-10-29 21:37:07 -0700

    Update commons.scala to resolve compiling issues for noah

--- examples/twittermap/project/commons.scala
+++ examples/twittermap/project/commons.scala
@@ -21,7 +21,10 @@ object Commons {
     assemblyMergeStrategy in assembly := {
       case PathList("org", "apache", "commons", "logging", xs@_*) => MergeStrategy.first
       case PathList("org", "apache", "commons", xs @_ *) => MergeStrategy.first
-      case "META-INF/io.netty.versions.properties" => MergeStrategy.last
+      //case "META-INF/io.netty.versions.properties" => MergeStrategy.last
+      case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+      case PathList("com", "sun", "activation", xs @_*) => MergeStrategy.first
+      case PathList("javax", "activation", xs @_*) => MergeStrategy.first
       case x =>
         val oldStrategy = (assemblyMergeStrategy in assembly).value
         oldStrategy(x)
