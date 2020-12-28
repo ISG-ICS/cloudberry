2455b69d70a45f50b55492304138e16af9125e94
Qiushi Bai <baiqiushi@gmail.com>
2020-12-27 21:21:09 -0800

    remove unnecessary comment

--- examples/twittermap/project/commons.scala
+++ examples/twittermap/project/commons.scala
@@ -21,7 +21,6 @@ object Commons {
     assemblyMergeStrategy in assembly := {
       case PathList("org", "apache", "commons", "logging", xs@_*) => MergeStrategy.first
       case PathList("org", "apache", "commons", xs @_ *) => MergeStrategy.first
-      //case "META-INF/io.netty.versions.properties" => MergeStrategy.last
       case PathList("META-INF", xs @ _*) => MergeStrategy.discard
       case PathList("com", "sun", "activation", xs @_*) => MergeStrategy.first
       case PathList("javax", "activation", xs @_*) => MergeStrategy.first
