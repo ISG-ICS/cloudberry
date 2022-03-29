62c67f4e037bcad647a14081bb547c300870e42d
Qiushi Bai <baiqiushi@gmail.com>
2022-03-29 16:40:33 -0700

    [twittermap] solve compiling lib resolution issue in project/commons.scala.

--- examples/twittermap/project/commons.scala
+++ examples/twittermap/project/commons.scala
@@ -24,9 +24,9 @@ object Commons {
       case PathList("META-INF", xs @ _*) => MergeStrategy.discard
       case PathList("com", "sun", "activation", xs @_*) => MergeStrategy.first
       case PathList("javax", "activation", xs @_*) => MergeStrategy.first
-      case x =>
-        val oldStrategy = (assemblyMergeStrategy in assembly).value
-        oldStrategy(x)
+      case x => MergeStrategy.first
+      //  val oldStrategy = (assemblyMergeStrategy in assembly).value
+      //  oldStrategy(x)
     }
   )
 
